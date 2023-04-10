dc2daf293f7da12137ad12eaa256a76eefbb793c
bruno13palhano <bruno13palhano@gmail.com>
2023-04-10 18:57:20 -0300

    Added ViewModel to naps

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/nap/NapsViewModel.kt
@@ -0,0 +1,23 @@
+package com.bruno13palhano.sleeptight.ui.nap
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.di.DefaultNapRep
+import com.bruno13palhano.core.data.repository.NapRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class NapsViewModel @Inject constructor(
+    @DefaultNapRep private val napRepository: NapRepository
+) : ViewModel() {
+
+    val uiState = napRepository.getAllStream()
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = emptyList(),
+            started = WhileSubscribed(5_000)
+        )
+}
