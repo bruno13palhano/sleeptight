94046f533b82b538b7de03ecdc896beef53a48f5
bruno13palhano <bruno13palhano@gmail.com>
2023-04-11 00:17:46 -0300

    Added application

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/dependencies/DependenciesApplication.kt
@@ -0,0 +1,7 @@
+package com.bruno13palhano.sleeptight.dependencies
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DependenciesApplication : Application()
