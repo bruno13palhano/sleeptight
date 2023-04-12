abaa814165c064649076590853d18bdde1099a71
bruno13palhano <bruno13palhano@gmail.com>
2023-04-12 11:28:56 -0300

    Fixed EditText hiden toolbar

--- app/src/main/java/com/bruno13palhano/sleeptight/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
