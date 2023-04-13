45402420d6dea963b7b6629548868a39c52d8dfd
bruno13palhano <bruno13palhano@gmail.com>
2023-04-13 16:06:44 -0300

    Refactored

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/newnap/NewNapViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/newnap/NewNapViewModel.kt
@@ -1,77 +1,69 @@
 package com.bruno13palhano.sleeptight.ui.newnap
 
+import android.icu.text.DateFormat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.di.DefaultNapRep
 import com.bruno13palhano.core.data.repository.NapRepository
 import com.bruno13palhano.model.Nap
+import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import java.util.Calendar
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class NewNapViewModel @Inject constructor(
     @DefaultNapRep private val napRepository: NapRepository
 ) : ViewModel() {
-    private val currentDate = Calendar.getInstance()
-    private val currentDay = currentDate.get(Calendar.DAY_OF_MONTH)
-    private val currentMonth = currentDate.get(Calendar.MONTH)
-    private val currentYear = currentDate.get(Calendar.YEAR)
-    private val currentMinute = currentDate.get(Calendar.MINUTE)
-    private val currentHour = currentDate.get(Calendar.HOUR)
+    private val currentDate = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
 
-    val day = MutableStateFlow(currentDay)
-    val month = MutableStateFlow(currentMonth)
-    val year = MutableStateFlow(currentYear)
-    val date = combine(day, month, year) { day, month, year ->
-        val calendar = Calendar.getInstance()
-        calendar.set(Calendar.DAY_OF_MONTH, day)
-        calendar.set(Calendar.MONTH, month)
-        calendar.set(Calendar.YEAR, year)
-
-        calendar.timeInMillis
-    }
+    val date = MutableStateFlow(currentDate.timeInMillis)
+    val dateUi = date.asStateFlow()
+        .map {
+            DateFormat.getPatternInstance(DateFormat.YEAR_ABBR_MONTH_WEEKDAY_DAY).format(it)
+        }
         .stateIn(
             scope = viewModelScope,
-            initialValue = 0L,
+            initialValue = "",
             started = WhileSubscribed(5_000)
         )
 
-    val startMinute = MutableStateFlow(currentMinute)
-    val startHour = MutableStateFlow(currentHour)
-    val startTime = combine(startMinute, startHour) { minute, hour ->
-        val calendar = Calendar.getInstance()
-        calendar.set(Calendar.MINUTE, minute)
-        calendar.set(Calendar.HOUR, hour)
-
-        calendar.timeInMillis
+    fun setDate(year: Int, month: Int, day: Int) {
+        date.value = dateToMilliseconds(year, month, day)
     }
+
+    val startTime = MutableStateFlow(currentDate.timeInMillis)
+    val startTimeUi = startTime.asStateFlow()
+        .map {
+            DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(it)
+        }
         .stateIn(
             scope = viewModelScope,
-            initialValue = 0L,
+            initialValue = "",
             started = WhileSubscribed(5_000)
         )
 
-    val endMinute = MutableStateFlow(currentMinute)
-    val endHour = MutableStateFlow(currentHour)
-    val endTime = combine(endMinute, endHour) { minute, hour ->
-        val calendar = Calendar.getInstance()
-        calendar.set(Calendar.MINUTE, minute)
-        calendar.set(Calendar.HOUR, hour)
-
-        calendar.timeInMillis
+    fun setStartTime(hour: Int, minute: Int) {
+        startTime.value = timeToMilliseconds(hour, minute)
     }
+
+    val endTime = MutableStateFlow(currentDate.timeInMillis)
+    val endTimeUi = endTime.asStateFlow()
+        .map {
+            DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(it)
+        }
         .stateIn(
             scope = viewModelScope,
-            initialValue = 0L,
+            initialValue = "",
             started = WhileSubscribed(5_000)
         )
+
+    fun setEndTime(hour: Int, minute: Int) {
+        endTime.value = timeToMilliseconds(hour, minute)
+    }
 
     val _observation = MutableStateFlow("")
     val observation = _observation.asStateFlow()
@@ -85,5 +77,22 @@ class NewNapViewModel @Inject constructor(
         viewModelScope.launch {
             napRepository.insert(nap)
         }
+    }
+
+    private fun dateToMilliseconds(year: Int, month: Int, day: Int): Long {
+        val calendar = android.icu.util.Calendar.getInstance(android.icu.util.TimeZone.getTimeZone("UTC"))
+        calendar[android.icu.util.Calendar.DAY_OF_MONTH] = day
+        calendar[android.icu.util.Calendar.MONTH] = month
+        calendar[android.icu.util.Calendar.YEAR] = year
+
+        return calendar.timeInMillis
+    }
+
+    private fun timeToMilliseconds(hour: Int, minute: Int): Long {
+        val calendar = android.icu.util.Calendar.getInstance()
+        calendar[android.icu.util.Calendar.HOUR_OF_DAY] = hour
+        calendar[android.icu.util.Calendar.MINUTE] = minute
+
+        return calendar.timeInMillis
     }
 }
