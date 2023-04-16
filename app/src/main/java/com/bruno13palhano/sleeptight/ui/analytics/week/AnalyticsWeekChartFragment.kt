ca67916050bcc60a0f709e1e38f87ceb839077c0
bruno13palhano <bruno13palhano@gmail.com>
2023-04-16 08:06:28 -0300

    Populate chart with average hours of sleep in the week

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/week/AnalyticsWeekChartFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/week/AnalyticsWeekChartFragment.kt
@@ -1,4 +1,4 @@
-package com.bruno13palhano.sleeptight.ui.analytics
+package com.bruno13palhano.sleeptight.ui.analytics.week
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,15 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentAnalyticsWeekChartBinding
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAStyle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class AnalyticsWeekChartFragment : Fragment() {
     private var _binding: FragmentAnalyticsWeekChartBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: AnalyticsWeekChartViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,25 +35,66 @@ class AnalyticsWeekChartFragment : Fragment() {
         val view = binding.root
 
         val chartModel: AAChartModel = AAChartModel()
-            .chartType(AAChartType.Area)
-            .title("Week Chart")
+            .chartType(AAChartType.Bar)
+            .title(getString(R.string.week_chart_label))
+            .titleStyle(AAStyle().color(getString(R.string.chart_primary_color)))
+            .subtitle(getString(R.string.average_hour_per_week_label))
+            .subtitleStyle(AAStyle().color(getString(R.string.chart_primary_color)))
             .dataLabelsEnabled(true)
             .backgroundColor(getString(R.string.chart_background_color))
+            .axesTextColor(getString(R.string.chart_primary_color))
+            .dataLabelsStyle(AAStyle().color(getString(R.string.chart_primary_color)))
             .series(
                 arrayOf(
                     AASeriesElement()
-                        .name("Sample 1")
-                        .data(arrayOf(1.2, 2.2, 3.3, 4.4))
-                        .color("#786e82"),
-                    AASeriesElement()
-                        .name("Sample 2")
-                        .data(arrayOf(4.2, 3.4, 2.3, 1.2))
-                        .color("#79c5c8")
+                        .name(getString(R.string.average_hours_label))
+                        .data(arrayOf(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F))
+                        .color(getString(R.string.chart_bar_main_color))
                 )
-            ).categories(arrayOf("Samples"))
-
+            ).categories(
+                arrayOf(
+                    getString(R.string.sunday_label),
+                    getString(R.string.monday_label),
+                    getString(R.string.tuesday_label),
+                    getString(R.string.wednesday_label),
+                    getString(R.string.thursday_label),
+                    getString(R.string.friday_label),
+                    getString(R.string.saturday_label)
+                )
+            )
         binding.weekChart.aa_drawChartWithChartModel(chartModel)
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.chartUi.collect {
+                    delay(200)
+                    binding.weekChart.aa_onlyRefreshTheChartDataWithChartOptionsSeriesArray(
+                        arrayOf(
+                            AASeriesElement()
+                                .name(getString(R.string.average_hours_label))
+                                .data(
+                                    arrayOf(
+                                        it.sunday,
+                                        it.monday,
+                                        it.tuesday,
+                                        it.wednesday,
+                                        it.thursday,
+                                        it.friday,
+                                        it.saturday
+                                    )
+                                )
+                                .color(getString(R.string.chart_bar_main_color))
+                        )
+                    )
+                }
+            }
+        }
+
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/week/AnalyticsWeekChartViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/week/AnalyticsWeekChartViewModel.kt
@@ -1,14 +1,96 @@
 package com.bruno13palhano.sleeptight.ui.analytics.week
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.di.DefaultNapRep
 import com.bruno13palhano.core.data.repository.NapRepository
+import com.bruno13palhano.model.Day
+import com.bruno13palhano.sleeptight.ui.analytics.averageSleepTimeDecimal
+import com.bruno13palhano.sleeptight.ui.analytics.hourToInt
+import com.bruno13palhano.sleeptight.ui.analytics.minuteToInt
+import com.bruno13palhano.sleeptight.ui.analytics.whichDay
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class AnalyticsWeekChartViewModel @Inject constructor(
     @DefaultNapRep private val napRepository: NapRepository
 ) : ViewModel() {
+    val chartUi = napRepository.getAllStream()
+        .map {
+            val sundayHours = mutableListOf<Int>()
+            val sundayMinutes = mutableListOf<Int>()
+            val mondayHours = mutableListOf<Int>()
+            val mondayMinutes = mutableListOf<Int>()
+            val tuesdayHours = mutableListOf<Int>()
+            val tuesdayMinute = mutableListOf<Int>()
+            val wednesdayHours = mutableListOf<Int>()
+            val wednesdayMinute = mutableListOf<Int>()
+            val thursdayHours = mutableListOf<Int>()
+            val thursdayMinute = mutableListOf<Int>()
+            val fridayHours = mutableListOf<Int>()
+            val fridayMinutes = mutableListOf<Int>()
+            val saturdayHours = mutableListOf<Int>()
+            val saturdayMinutes = mutableListOf<Int>()
 
+            it.forEach { nap ->
+                when (whichDay(nap.date)) {
+                    Day.SUNDAY -> {
+                        sundayHours.add(hourToInt(nap.sleepTime))
+                        sundayMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Day.MONDAY -> {
+                        mondayHours.add(hourToInt(nap.sleepTime))
+                        mondayMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Day.TUESDAY -> {
+                        tuesdayHours.add(hourToInt(nap.sleepTime))
+                        tuesdayMinute.add(minuteToInt(nap.sleepTime))
+                    }
+                    Day.WEDNESDAY -> {
+                        wednesdayHours.add(hourToInt(nap.sleepTime))
+                        wednesdayMinute.add(minuteToInt(nap.sleepTime))
+                    }
+                    Day.THURSDAY -> {
+                        thursdayHours.add(hourToInt(nap.sleepTime))
+                        thursdayMinute.add(minuteToInt(nap.sleepTime))
+                    }
+                    Day.FRIDAY -> {
+                        fridayHours.add(hourToInt(nap.sleepTime))
+                        fridayMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Day.SATURDAY -> {
+                        saturdayHours.add(hourToInt(nap.sleepTime))
+                        saturdayMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                }
+            }
+            MonthChartUi(
+                sunday = averageSleepTimeDecimal(sundayHours, sundayMinutes),
+                monday = averageSleepTimeDecimal(mondayHours, mondayMinutes),
+                tuesday = averageSleepTimeDecimal(tuesdayHours, tuesdayMinute),
+                wednesday = averageSleepTimeDecimal(wednesdayHours, wednesdayMinute),
+                thursday = averageSleepTimeDecimal(thursdayHours, thursdayMinute),
+                friday = averageSleepTimeDecimal(fridayHours, fridayMinutes),
+                saturday = averageSleepTimeDecimal(saturdayHours, saturdayMinutes)
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = MonthChartUi(),
+            started = SharingStarted.WhileSubscribed(5_000)
+        )
+
+    data class MonthChartUi(
+        val sunday: Float = 0.0F,
+        val monday: Float = 0.0F,
+        val tuesday: Float = 0.0F,
+        val wednesday: Float = 0.0F,
+        val thursday: Float = 0.0F,
+        val friday: Float = 0.0F,
+        val saturday: Float = 0.0F
+    )
 }
