63899b4484922b29dad06bb531c9da482935ef09
bruno13palhano <bruno13palhano@gmail.com>
2023-04-16 12:35:03 -0300

    Populate chart with average hours of sleep in the month

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/month/AnalyticsMonthChartFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/month/AnalyticsMonthChartFragment.kt
@@ -1,4 +1,4 @@
-package com.bruno13palhano.sleeptight.ui.analytics
+package com.bruno13palhano.sleeptight.ui.analytics.month
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,15 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentAnalyticsMonthChartBinding
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAStyle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class AnalyticsMonthChartFragment : Fragment() {
     private var _binding: FragmentAnalyticsMonthChartBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: AnalyticsMonthChartViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,23 +36,68 @@ class AnalyticsMonthChartFragment : Fragment() {
 
         val chartModel: AAChartModel = AAChartModel()
             .chartType(AAChartType.Column)
-            .title("Month Chart")
+            .title(getString(R.string.month_chart_label))
+            .titleStyle(AAStyle().color(getString(R.string.chart_primary_color)))
+            .subtitle(getString(R.string.average_hour_per_month_label))
+            .subtitleStyle(AAStyle().color(getString(R.string.chart_primary_color)))
             .dataLabelsEnabled(true)
             .backgroundColor(getString(R.string.chart_background_color))
+            .axesTextColor(getString(R.string.chart_primary_color))
+            .dataLabelsStyle(AAStyle().color(getString(R.string.chart_primary_color)))
             .series(
                 arrayOf(
                     AASeriesElement()
-                        .name("Sample 1")
-                        .data(arrayOf(1.2, 2.2, 3.3, 4.4))
-                        .color("#786e82"),
-                    AASeriesElement()
-                        .name("Sample 2")
-                        .data(arrayOf(4.2, 3.4, 2.3, 1.2))
-                        .color("#79c5c8")
+                        .name(getString(R.string.average_hours_label))
+                        .data(arrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+                        .color(getString(R.string.chart_bar_main_color)),
                 )
-            ).categories(arrayOf("Samples"))
+            ).categories(
+                arrayOf(
+                    getString(R.string.january_label),
+                    getString(R.string.february_label),
+                    getString(R.string.march_label),
+                    getString(R.string.april_label),
+                    getString(R.string.may_label),
+                    getString(R.string.june_label),
+                    getString(R.string.july_label),
+                    getString(R.string.august_label),
+                    getString(R.string.september_label),
+                    getString(R.string.october_label),
+                    getString(R.string.november_label),
+                    getString(R.string.december_label)
+                )
+            )
 
         binding.monthChart.aa_drawChartWithChartModel(chartModel)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.chartUi.collect {
+                    delay(200)
+                    binding.monthChart.aa_onlyRefreshTheChartDataWithChartOptionsSeriesArray(
+                        arrayOf(
+                            AASeriesElement()
+                                .name(getString(R.string.month_chart_label))
+                                .data(arrayOf(
+                                    it.january,
+                                    it.february,
+                                    it.march,
+                                    it.april,
+                                    it.may,
+                                    it.june,
+                                    it.july,
+                                    it.august,
+                                    it.september,
+                                    it.october,
+                                    it.november,
+                                    it.december
+                                ))
+                                .color(getString(R.string.chart_bar_main_color))
+                        )
+                    )
+                }
+            }
+        }
 
         return view
     }
--- app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/month/AnalyticsMonthChartViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/month/AnalyticsMonthChartViewModel.kt
@@ -1,15 +1,137 @@
 package com.bruno13palhano.sleeptight.ui.analytics.month
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.di.DefaultNapRep
 import com.bruno13palhano.core.data.repository.NapRepository
+import com.bruno13palhano.model.Month
+import com.bruno13palhano.sleeptight.ui.analytics.averageSleepTimeDecimal
+import com.bruno13palhano.sleeptight.ui.analytics.hourToInt
+import com.bruno13palhano.sleeptight.ui.analytics.minuteToInt
+import com.bruno13palhano.sleeptight.ui.analytics.whichMonth
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class AnalyticsMonthViewModel @Inject constructor(
+class AnalyticsMonthChartViewModel @Inject constructor(
     @DefaultNapRep private val napRepository: NapRepository
 ) : ViewModel() {
+    val chartUi = napRepository.getAllStream()
+        .map {
+            val januaryHours = mutableListOf<Int>()
+            val januaryMinutes = mutableListOf<Int>()
+            val februaryHours = mutableListOf<Int>()
+            val februaryMinutes = mutableListOf<Int>()
+            val marchHours = mutableListOf<Int>()
+            val marchMinutes = mutableListOf<Int>()
+            val aprilHours = mutableListOf<Int>()
+            val aprilMinutes = mutableListOf<Int>()
+            val mayHours = mutableListOf<Int>()
+            val mayMinutes = mutableListOf<Int>()
+            val juneHours = mutableListOf<Int>()
+            val juneMinutes = mutableListOf<Int>()
+            val julyHours = mutableListOf<Int>()
+            val julyMinutes = mutableListOf<Int>()
+            val augustHours = mutableListOf<Int>()
+            val augustMinutes = mutableListOf<Int>()
+            val septemberHours = mutableListOf<Int>()
+            val septemberMinutes = mutableListOf<Int>()
+            val octoberHours = mutableListOf<Int>()
+            val octoberMinutes = mutableListOf<Int>()
+            val novemberHours = mutableListOf<Int>()
+            val novemberMinutes = mutableListOf<Int>()
+            val decemberHours = mutableListOf<Int>()
+            val decemberMinutes = mutableListOf<Int>()
 
+            it.forEach { nap ->
+                when (whichMonth(nap.date)) {
+                    Month.JANUARY -> {
+                        januaryHours.add(hourToInt(nap.sleepTime))
+                        januaryMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.FEBRUARY -> {
+                        februaryHours.add(hourToInt(nap.sleepTime))
+                        februaryMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.MARCH -> {
+                        marchHours.add(hourToInt(nap.sleepTime))
+                        marchMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.APRIL -> {
+                        aprilHours.add(hourToInt(nap.sleepTime))
+                        aprilMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.MAY -> {
+                        mayHours.add(hourToInt(nap.sleepTime))
+                        mayMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.JUNE -> {
+                        juneHours.add(hourToInt(nap.sleepTime))
+                        juneMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.JULY -> {
+                        julyHours.add(hourToInt(nap.sleepTime))
+                        julyMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.AUGUST -> {
+                        augustHours.add(hourToInt(nap.sleepTime))
+                        augustMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.SEPTEMBER -> {
+                        septemberHours.add(hourToInt(nap.sleepTime))
+                        septemberMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.OCTOBER -> {
+                        octoberHours.add(hourToInt(nap.sleepTime))
+                        octoberMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.NOVEMBER -> {
+                        novemberHours.add(hourToInt(nap.sleepTime))
+                        novemberMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                    Month.DECEMBER -> {
+                        decemberHours.add(hourToInt(nap.sleepTime))
+                        decemberMinutes.add(minuteToInt(nap.sleepTime))
+                    }
+                }
+            }
 
+            MonthChartUi(
+                january = averageSleepTimeDecimal(januaryHours, januaryMinutes),
+                february = averageSleepTimeDecimal(februaryHours, februaryMinutes),
+                march = averageSleepTimeDecimal(marchHours, marchMinutes),
+                april = averageSleepTimeDecimal(aprilHours, aprilMinutes),
+                may = averageSleepTimeDecimal(mayHours, mayMinutes),
+                june = averageSleepTimeDecimal(juneHours, juneMinutes),
+                july = averageSleepTimeDecimal(julyHours, julyMinutes),
+                august = averageSleepTimeDecimal(augustHours, augustMinutes),
+                september = averageSleepTimeDecimal(septemberHours, septemberMinutes),
+                october = averageSleepTimeDecimal(octoberHours, octoberMinutes),
+                november = averageSleepTimeDecimal(novemberHours, novemberMinutes),
+                december = averageSleepTimeDecimal(decemberHours, decemberMinutes)
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = MonthChartUi(),
+            started = WhileSubscribed(5_000)
+        )
+
+    data class MonthChartUi(
+        val january: Float =  0.0F,
+        val february: Float = 0.0F,
+        val march: Float = 0.0F,
+        val april: Float = 0.0F,
+        val may: Float = 0.0F,
+        val june: Float = 0.0F,
+        val july: Float = 0.0F,
+        val august: Float = 0.0F,
+        val september: Float = 0.0F,
+        val october: Float = 0.0F,
+        val november: Float = 0.0F,
+        val december: Float = 0.0F
+    )
 }
