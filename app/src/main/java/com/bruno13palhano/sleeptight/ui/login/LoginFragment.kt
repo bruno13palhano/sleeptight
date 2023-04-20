285d25efc17b8b2074035628f9636a1dd0250ed8
bruno13palhano <bruno13palhano@gmail.com>
2023-04-20 12:16:47 -0300

    Added LoginFragment

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/login/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.bruno13palhano.sleeptight.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bruno13palhano.sleeptight.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
