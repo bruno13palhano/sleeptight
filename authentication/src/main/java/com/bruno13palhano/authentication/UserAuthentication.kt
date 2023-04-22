2d0d7eb2c7fe0b20aa3aa0a439d277b12088b017
bruno13palhano <bruno13palhano@gmail.com>
2023-04-22 10:46:48 -0300

    Added a user authentication interface

---
+++ authentication/src/main/java/com/bruno13palhano/authentication/UserAuthentication.kt
@@ -0,0 +1,16 @@
+package com.bruno13palhano.authentication
+
+import android.graphics.Bitmap
+import com.google.firebase.firestore.auth.User
+
+interface UserAuthentication {
+    fun createUser(user: User, onSuccess: (userUid: String) -> Unit, onFail: () -> Unit)
+    fun login(email: String, password: String, onSuccess: () -> Unit, onFail: () -> Unit)
+    fun logout()
+    fun isUserAuthenticated(): Boolean
+    fun getCurrentUser(): User
+    fun updateUserUrlPhoto(photo: Bitmap, onSuccess: (newPhotoUrl: String, userUid: String) -> Unit,
+        onFail: () -> Unit)
+    fun updateUsername(username: String, onSuccess: (newUsername: String, userUid: String) -> Unit,
+        onFail: () -> Unit)
+}
