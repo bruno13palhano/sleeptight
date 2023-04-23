6f5ce873f43aab79f724d625b917d8d7ed4df814
bruno13palhano <bruno13palhano@gmail.com>
2023-04-23 19:30:50 -0300

    Added a User model to data layer

---
+++ core/src/main/java/com/bruno13palhano/core/data/database/model/UserData.kt
@@ -0,0 +1,67 @@
+package com.bruno13palhano.core.data.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.bruno13palhano.model.User
+
+@Entity(tableName = "user_table")
+internal data class UserData(
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: String,
+
+    @ColumnInfo(name = "username")
+    val username: String,
+
+    @ColumnInfo(name = "email")
+    val email: String,
+
+    @ColumnInfo(name = "baby_name")
+    val babyName: String,
+
+    @ColumnInfo(name = "baby_url_photo")
+    val babyUrlPhoto: String,
+
+    @ColumnInfo(name = "birth_place")
+    val birthplace: String,
+
+    @ColumnInfo(name = "birth_date")
+    val birthdate: Long,
+
+    @ColumnInfo(name = "birth_time")
+    val birthTime: Long,
+
+    @ColumnInfo(name = "height")
+    val height: Float,
+
+    @ColumnInfo(name = "weight")
+    val weight: Float
+)
+
+internal fun UserData.asUser() = User(
+    id = id,
+    username = username,
+    email = email,
+    babyName = babyName,
+    babyUrlPhoto = babyUrlPhoto,
+    birthplace = birthplace,
+    birthdate = birthdate,
+    birthTime = birthTime,
+    height = height,
+    weight = weight
+)
+
+internal fun User.asUserData() = UserData(
+    id = id,
+    username = username,
+    email = email,
+    babyName = babyName,
+    babyUrlPhoto = babyUrlPhoto,
+    birthplace = birthplace,
+    birthdate = birthdate,
+    birthTime = birthTime,
+    height = height,
+    weight = weight
+)
