d414faeffd918e0bf36940e0dffe1bbf2aec965f
bruno13palhano <bruno13palhano@gmail.com>
2023-04-23 20:48:15 -0300

    Added UserData and UserDao to database

--- core/src/main/java/com/bruno13palhano/core/data/database/SleepTightDatabase.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/SleepTightDatabase.kt
@@ -3,15 +3,19 @@ package com.bruno13palhano.core.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.bruno13palhano.core.data.database.dao.NapDao
+import com.bruno13palhano.core.data.database.dao.UserDao
 import com.bruno13palhano.core.data.database.model.NapData
+import com.bruno13palhano.core.data.database.model.UserData
 
 @Database(
     entities = [
-        NapData::class
+        NapData::class,
+        UserData::class,
     ],
     version = 1,
     exportSchema = false
 )
 internal abstract class SleepTightDatabase : RoomDatabase() {
     abstract val napDao: NapDao
+    abstract val userDao: UserDao
 }
