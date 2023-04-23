bd5c5f85b9b135eaea1f2039585b34d27bf0e1df
bruno13palhano <bruno13palhano@gmail.com>
2023-04-23 18:39:52 -0300

    Extracted model and dao to packages

--- core/src/main/java/com/bruno13palhano/core/data/database/SleepTightDatabase.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/SleepTightDatabase.kt
@@ -2,6 +2,7 @@ package com.bruno13palhano.core.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.bruno13palhano.core.data.database.dao.NapDao
 
 @Database(
     entities = [
