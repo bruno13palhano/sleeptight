cc05ddc12230afeca4961da0e52a2875351f7544
bruno13palhano <bruno13palhano@gmail.com>
2023-04-24 09:48:14 -0300

    Added buttons

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/AnalyticsFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/AnalyticsFragment.kt
@@ -5,14 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.bruno13palhano.sleeptight.R
+import com.bruno13palhano.sleeptight.databinding.FragmentAnalyticsBinding
 
 class AnalyticsFragment : Fragment() {
+    private var _binding: FragmentAnalyticsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_analytics, container, false)
+    ): View {
+        _binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_analytics, container, false)
+        val view = binding.root
+
+        binding.uiEvents = this
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun navigateToWeekChart() {
+        findNavController().navigate(
+            AnalyticsFragmentDirections.actionAnalyticsToWeek())
+    }
+
+    fun navigateToMonthChart() {
+        findNavController().navigate(
+            AnalyticsFragmentDirections.actionAnalyticsToMonth())
     }
 }
