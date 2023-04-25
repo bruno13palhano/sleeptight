cf429d4a213bc818670628c249cdfce8cb58bddd
bruno13palhano <bruno13palhano@gmail.com>
2023-04-25 15:21:50 -0300

    Insert user in database when make login

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/login/LoginViewModel.kt
@@ -1,16 +1,22 @@
 package com.bruno13palhano.sleeptight.ui.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.authentication.DefaultUserFirebase
 import com.bruno13palhano.authentication.UserAuthentication
+import com.bruno13palhano.core.data.di.DefaultUserRep
+import com.bruno13palhano.core.data.repository.UserRepository
+import com.bruno13palhano.model.User
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    @DefaultUserFirebase private val authentication: UserAuthentication
+    @DefaultUserFirebase private val authentication: UserAuthentication,
+    @DefaultUserRep private val userRepository: UserRepository
 ) : ViewModel() {
     private val _loginStatus = MutableStateFlow<LoginStatus>(LoginStatus.Default)
     val loginStatus = _loginStatus.asStateFlow()
@@ -24,7 +30,8 @@ class LoginViewModel @Inject constructor(
             authentication.login(
                 email = email,
                 password = password,
-                onSuccess = {
+                onSuccess = { user ->
+                    insertUserInDatabase(user)
                     _loginStatus.value = LoginStatus.Success
                 },
                 onFail = {
@@ -36,6 +43,12 @@ class LoginViewModel @Inject constructor(
 
     private fun isValuesValid(email: String, password: String): Boolean =
         email != "" && password != ""
+
+    private fun insertUserInDatabase(user: User) {
+        viewModelScope.launch {
+            userRepository.insertUser(user)
+        }
+    }
 
     private fun loading() {
         _loginStatus.value = LoginStatus.Loading
