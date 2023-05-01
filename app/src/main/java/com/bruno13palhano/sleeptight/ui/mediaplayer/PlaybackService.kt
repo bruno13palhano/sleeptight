8ddc67bfb05d32c4670be3cddb05500862188fdf
bruno13palhano <bruno13palhano@gmail.com>
2023-05-01 16:00:25 -0300

    Added a PlaybackService

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/mediaplayer/PlaybackService.kt
@@ -0,0 +1,294 @@
+package com.bruno13palhano.sleeptight.ui.mediaplayer
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Intent
+import android.os.Bundle
+import android.app.TaskStackBuilder
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.*
+import com.bruno13palhano.sleeptight.MainActivity
+import com.google.common.collect.ImmutableList
+import com.google.common.util.concurrent.ListenableFuture
+import com.bruno13palhano.sleeptight.R
+import com.google.common.util.concurrent.Futures
+
+
+@UnstableApi class PlaybackService : MediaLibraryService() {
+    private val librarySessionCallback = CustomMediaLibrarySessionCallback()
+
+    private lateinit var player: ExoPlayer
+    private lateinit var mediaLibrarySession: MediaLibrarySession
+    private lateinit var customCommands: List<CommandButton>
+
+    private var customLayout = ImmutableList.of<CommandButton>()
+
+    companion object {
+        private const val SEARCH_QUERY_PREFIX_COMPAT = "androidx://media3-session/playFromSearch"
+        private const val SEARCH_QUERY_PREFIX = "androidx://media3-session/setMediaUri"
+        private const val CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_ON =
+            "android.media3.session.demo.SHUFFLE_ON"
+        private const val CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_OFF =
+            "android.media3.session.demo.SHUFFLE_OFF"
+        private const val NOTIFICATION_ID = 123
+        private const val CHANNEL_ID = "demo_session_notification_channel_id"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        customCommands = listOf(
+            getShuffleCommandButton(
+                SessionCommand(CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_ON, Bundle.EMPTY)
+            ),
+            getShuffleCommandButton(
+                SessionCommand(CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_OFF, Bundle.EMPTY)
+            )
+        )
+        customLayout = ImmutableList.of(customCommands[0])
+        initializeSessionAndPlayer()
+        setListener(MediaSessionServiceListener())
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaLibrarySession {
+        return mediaLibrarySession
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        if (!player.playWhenReady) {
+            stopSelf()
+        }
+    }
+
+    override fun onDestroy() {
+        player.release()
+        mediaLibrarySession.release()
+        clearListener()
+        super.onDestroy()
+    }
+
+    private inner class CustomMediaLibrarySessionCallback : MediaLibrarySession.Callback {
+        override fun onConnect(
+            session: MediaSession,
+            controller: MediaSession.ControllerInfo
+        ): MediaSession.ConnectionResult {
+            val connectionResult = super.onConnect(session, controller)
+            val availableSessionCommands = connectionResult.availableSessionCommands.buildUpon()
+
+            customCommands.forEach { commandButton ->
+                commandButton.sessionCommand?.let { availableSessionCommands.add(it) }
+            }
+
+            return MediaSession.ConnectionResult.accept(
+                availableSessionCommands.build(),connectionResult.availablePlayerCommands
+            )
+        }
+
+        override fun onPostConnect(session: MediaSession, controller: MediaSession.ControllerInfo) {
+            if (!customLayout.isEmpty() && controller.controllerVersion != 0) {
+                ignoreFuture(mediaLibrarySession.setCustomLayout(controller, customLayout))
+            }
+        }
+
+        override fun onCustomCommand(
+            session: MediaSession,
+            controller: MediaSession.ControllerInfo,
+            customCommand: SessionCommand,
+            args: Bundle
+        ): ListenableFuture<SessionResult> {
+            if (CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_ON == customCommand.customAction) {
+                player.shuffleModeEnabled = true
+                customLayout = ImmutableList.of(customCommands[1])
+                session.setCustomLayout(customLayout)
+            } else if (CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_OFF == customCommand.customAction) {
+                player.shuffleModeEnabled = false
+                customLayout = ImmutableList.of(customCommands[0])
+                session.setCustomLayout(customLayout)
+            }
+
+            return Futures.immediateFuture(SessionResult(SessionResult.RESULT_SUCCESS))
+        }
+
+        override fun onGetLibraryRoot(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<MediaItem>> {
+            return Futures.immediateFuture(LibraryResult.ofItem(MediaItemTree.getRootItem(), params))
+        }
+
+        override fun onGetItem(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            mediaId: String
+        ): ListenableFuture<LibraryResult<MediaItem>> {
+            val item = MediaItemTree.getItem(mediaId)
+                ?: return Futures.immediateFuture(
+                    LibraryResult.ofError(LibraryResult.RESULT_ERROR_BAD_VALUE)
+                )
+            return Futures.immediateFuture(LibraryResult.ofItem(item, null))
+        }
+
+        override fun onSubscribe(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            parentId: String,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<Void>> {
+            val children = MediaItemTree.getChildren(parentId)
+                ?: return Futures.immediateFuture(
+                    LibraryResult.ofError(LibraryResult.RESULT_ERROR_BAD_VALUE)
+                )
+            session.notifyChildrenChanged(browser, parentId, children.size, params)
+            return Futures.immediateFuture(LibraryResult.ofVoid())
+        }
+
+        override fun onGetChildren(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            parentId: String,
+            page: Int,
+            pageSize: Int,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<ImmutableList<MediaItem>>> {
+            val children = MediaItemTree.getChildren(parentId)
+                ?: return Futures.immediateFuture(
+                    LibraryResult.ofError(LibraryResult.RESULT_ERROR_BAD_VALUE)
+                )
+
+            return Futures.immediateFuture(LibraryResult.ofItemList(children, params))
+        }
+
+        override fun onAddMediaItems(
+            mediaSession: MediaSession,
+            controller: MediaSession.ControllerInfo,
+            mediaItems: MutableList<MediaItem>
+        ): ListenableFuture<List<MediaItem>> {
+            val updateMediaItems: List<MediaItem> = mediaItems.map { mediaItem ->
+                if (mediaItem.requestMetadata.searchQuery != null)
+                    getMediaItemFromSearchQuery(mediaItem.requestMetadata.searchQuery!!)
+                else MediaItemTree.getItem(mediaItem.mediaId) ?: mediaItem
+
+            }
+            return Futures.immediateFuture(updateMediaItems)
+        }
+
+        private fun getMediaItemFromSearchQuery(query: String): MediaItem {
+            val mediaTitle =
+                if (query.startsWith("play", ignoreCase = true)) {
+                    query.drop(5)
+                } else {
+                    query
+                }
+
+            return MediaItemTree.getItemFromTitle(mediaTitle) ?: MediaItemTree.getRandomItem()
+        }
+    }
+
+    private fun getShuffleCommandButton(sessionCommand: SessionCommand): CommandButton {
+        val isOn = sessionCommand.customAction == CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_ON
+        return CommandButton.Builder()
+            .setDisplayName(
+                getString(
+                    if (isOn) R.string.shuffle_on_description
+                    else R.string.shuffle_off_description
+                )
+            )
+            .setSessionCommand(sessionCommand)
+            .setIconResId(if (isOn) R.drawable.baseline_shuffle_24 else R.drawable.baseline_shuffle_on_24)
+            .build()
+    }
+
+    private fun initializeSessionAndPlayer() {
+        player = ExoPlayer.Builder(this)
+            .setAudioAttributes(AudioAttributes.DEFAULT, true)
+            .build()
+        MediaItemTree.initialize()
+
+        val items = MediaItemTree.getChildren("[artist]Gusttavo Lima")
+        if (items != null) {
+            player.addMediaItems(items)
+//            player.prepare()
+//            player.play()
+        }
+
+        val sessionActivityPendingIntent =
+            TaskStackBuilder.create(this).run {
+                addNextIntent(Intent(this@PlaybackService, MainActivity::class.java))
+
+                getPendingIntent(0, FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT)
+            }
+
+        mediaLibrarySession =
+            MediaLibrarySession.Builder(this, player, librarySessionCallback)
+                .setSessionActivity(sessionActivityPendingIntent)
+                .build()
+        if (!customLayout.isEmpty()) {
+            mediaLibrarySession.setCustomLayout(customLayout)
+        }
+    }
+
+    private fun ignoreFuture(customLayout: ListenableFuture<SessionResult>) {
+
+    }
+
+    @UnstableApi private inner class MediaSessionServiceListener : Listener {
+        override fun onForegroundServiceStartNotAllowedException() {
+            val notificationManagerCompat = NotificationManagerCompat.from(this@PlaybackService)
+            ensureNotificationChannel(notificationManagerCompat)
+            val pendingIntent =
+                TaskStackBuilder.create(this@PlaybackService).run {
+                    addNextIntent(Intent(this@PlaybackService, MainActivity::class.java))
+
+                    getPendingIntent(0, FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT)
+                }
+            val builder =
+                NotificationCompat.Builder(this@PlaybackService, CHANNEL_ID)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(androidx.media3.session.R.drawable.media3_notification_small_icon)
+                    .setContentTitle(getString(R.string.notification_content_title))
+                    .setStyle(
+                        NotificationCompat.BigTextStyle().bigText(getString(R.string.notification_content_text))
+                    )
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true)
+            if (ActivityCompat.checkSelfPermission(
+                    this@PlaybackService,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            notificationManagerCompat.notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    private fun ensureNotificationChannel(notificationManagerCompat: NotificationManagerCompat) {
+        if (Util.SDK_INT < 26 || notificationManagerCompat.getNotificationChannel(CHANNEL_ID) != null)
+            return
+
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            getString(R.string.notification_channel_name),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManagerCompat.createNotificationChannel(channel)
+    }
+}
