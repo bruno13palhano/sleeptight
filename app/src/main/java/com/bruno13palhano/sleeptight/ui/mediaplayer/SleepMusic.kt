c7adad9ed50aec5d94e3bfa7f581ec7416cd8a11
bruno13palhano <bruno13palhano@gmail.com>
2023-05-01 15:57:32 -0300

    Create a class to wrap a music

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/mediaplayer/SleepMusic.kt
@@ -0,0 +1,13 @@
+package com.bruno13palhano.sleeptight.ui.mediaplayer
+
+import android.net.Uri
+
+data class SleepMusic(
+    val id: String,
+    val title: String,
+    val album: String,
+    val artist: String,
+    val genre: String,
+    val source: Uri?,
+    val image: Uri?
+)
