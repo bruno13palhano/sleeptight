d75dd7ffdcfdc5305f3d1b6fbccb4874e8689277
bruno13palhano <bruno13palhano@gmail.com>
2023-05-03 18:31:54 -0300

    Created BabyStatusRepository

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/BabyStatusRepository.kt
@@ -0,0 +1,12 @@
+package com.bruno13palhano.core.data.repository
+
+import com.bruno13palhano.model.BabyStatus
+import kotlinx.coroutines.flow.Flow
+
+interface BabyStatusRepository {
+    suspend fun insertBabyStatus(babyStatus: BabyStatus)
+    fun getAllStream(): Flow<List<BabyStatus>>
+    fun getBabyStatusByIdStream(id: Long): Flow<BabyStatus>
+    suspend fun updateBabyStatus(babyStatus: BabyStatus)
+    suspend fun deleteBabyStatusById(id: Long)
+}
---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/DefaultBabyStatusRepository.kt
@@ -0,0 +1,38 @@
+package com.bruno13palhano.core.data.repository
+
+import com.bruno13palhano.core.data.database.dao.BabyStatusDao
+import com.bruno13palhano.core.data.database.model.asBabyStatus
+import com.bruno13palhano.core.data.database.model.asBabyStatusData
+import com.bruno13palhano.model.BabyStatus
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+internal class DefaultBabyStatusRepository(
+    private val babyStatusDao: BabyStatusDao
+) : BabyStatusRepository {
+    override suspend fun insertBabyStatus(babyStatus: BabyStatus) {
+        babyStatusDao.insert(babyStatus.asBabyStatusData())
+    }
+
+    override fun getAllStream(): Flow<List<BabyStatus>> {
+        return babyStatusDao.getAllStream().map {
+            it.map { babyStatusData ->
+                babyStatusData.asBabyStatus()
+            }
+        }
+    }
+
+    override fun getBabyStatusByIdStream(id: Long): Flow<BabyStatus> {
+        return babyStatusDao.getBabyStatusByIdStream(id).map {
+            it.asBabyStatus()
+        }
+    }
+
+    override suspend fun updateBabyStatus(babyStatus: BabyStatus) {
+        babyStatusDao.updateBabyStatus(babyStatus.asBabyStatusData())
+    }
+
+    override suspend fun deleteBabyStatusById(id: Long) {
+        babyStatusDao.deleteBabyStatusById(id)
+    }
+}
