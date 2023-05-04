ef7a8b8ffbb8b099bcc8d116240925ba8e2af2f2
bruno13palhano <bruno13palhano@gmail.com>
2023-05-04 18:45:11 -0300

    Added BabyStatusRepository

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeViewModel.kt
@@ -1,15 +1,28 @@
 package com.bruno13palhano.sleeptight.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.authentication.DefaultUserFirebase
 import com.bruno13palhano.authentication.UserAuthentication
+import com.bruno13palhano.core.data.di.DefaultBabyStatusRep
+import com.bruno13palhano.core.data.repository.BabyStatusRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    @DefaultUserFirebase private val authentication: UserAuthentication
+    @DefaultUserFirebase private val authentication: UserAuthentication,
+    @DefaultBabyStatusRep private val babyStatusRepository: BabyStatusRepository
 ) : ViewModel() {
+
+    val allBabyStatus = babyStatusRepository.getAllStream()
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = emptyList(),
+            started = WhileSubscribed(5_000)
+        )
 
     fun isUserAuthenticated(): Boolean =
         authentication.isUserAuthenticated()
