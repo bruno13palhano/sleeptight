3eca45393f899f8348d2ededd4c400fc4c04e186
bruno13palhano <bruno13palhano@gmail.com>
2023-05-03 21:12:53 -0300

    Added dataBinding

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/BabyStatusFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/BabyStatusFragment.kt
@@ -5,14 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.bruno13palhano.sleeptight.R
+import com.bruno13palhano.sleeptight.databinding.FragmentBabyStatusBinding
 
 class BabyStatusFragment : Fragment() {
+    private var _binding: FragmentBabyStatusBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_baby_status, container, false)
+    ): View {
+        _binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_baby_status, container, false)
+        val view = binding.root
+
+        binding.uiEvents = this
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
