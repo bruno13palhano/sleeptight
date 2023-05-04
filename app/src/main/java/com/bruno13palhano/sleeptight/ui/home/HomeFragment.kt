e72cd3559d5c5faec490d909d36ddb9366994bc9
bruno13palhano <bruno13palhano@gmail.com>
2023-05-04 18:48:07 -0300

    Navigate to BabyStatusFragment on item click

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeFragment.kt
@@ -7,11 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import com.bruno13palhano.model.BabyStatus
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -34,70 +37,17 @@ class HomeFragment : Fragment() {
         }
 
         val adapter = BabyStatusAdapter {
-
+            navigateToBabyStatus(it)
         }
         binding.list.adapter = adapter
 
-        val status = listOf(
-            BabyStatus(
-                id = 1L,
-                title = "Test 1",
-                date = 1L,
-                height = 43.3F,
-                weight = 2.23F,
-            ),
-            BabyStatus(
-                id = 2L,
-                title = "Test 2",
-                date = 2L,
-                height = 45.2F,
-                weight = 2.43F,
-            ),
-            BabyStatus(
-                id = 3L,
-                title = "Test 3",
-                date = 3L,
-                height = 47.1F,
-                weight = 2.98F,
-            ),
-            BabyStatus(
-                id = 4L,
-                title = "Test 4",
-                date = 4L,
-                height = 53.3F,
-                weight = 3.23F,
-            ),
-            BabyStatus(
-                id = 5L,
-                title = "Test 5",
-                date = 5L,
-                height = 56.3F,
-                weight = 3.63F,
-            ),
-            BabyStatus(
-                id = 6L,
-                title = "Test 6",
-                date = 6L,
-                height = 57.3F,
-                weight = 3.73F,
-            ),
-            BabyStatus(
-                id = 7L,
-                title = "Test 7",
-                date = 7L,
-                height = 61.5F,
-                weight = 4.1F,
-            ),
-            BabyStatus(
-                id = 8L,
-                title = "Test 8",
-                date = 8L,
-                height = 64.1F,
-                weight = 4.43F,
-            ),
-        )
-
-        adapter.submitList(status)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.allBabyStatus.collect {
+                    adapter.submitList(it)
+                }
+            }
+        }
 
         return view
     }
@@ -115,5 +65,10 @@ class HomeFragment : Fragment() {
     fun navigateToNewBabyStatus() {
         findNavController().navigate(
             HomeFragmentDirections.actionHomeToBabyStatusTitleAndDate())
+    }
+
+    private fun navigateToBabyStatus(id: Long) {
+        findNavController().navigate(
+            HomeFragmentDirections.actionHomeToBabyStatus(id))
     }
 }
