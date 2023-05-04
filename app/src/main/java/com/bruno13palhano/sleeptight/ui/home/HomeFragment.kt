5cde010784f2a411d2b61e2abca1bd817149b56f
bruno13palhano <bruno13palhano@gmail.com>
2023-05-03 21:11:38 -0300

    Added a method to navigate to NewBabyStatusTitleAndDateFragment

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.bruno13palhano.model.BabyStatus
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,6 +33,72 @@ class HomeFragment : Fragment() {
             navigateToLogin()
         }
 
+        val adapter = BabyStatusAdapter {
+
+        }
+        binding.list.adapter = adapter
+
+        val status = listOf(
+            BabyStatus(
+                id = 1L,
+                title = "Test 1",
+                date = 1L,
+                height = 43.3F,
+                weight = 2.23F,
+            ),
+            BabyStatus(
+                id = 2L,
+                title = "Test 2",
+                date = 2L,
+                height = 45.2F,
+                weight = 2.43F,
+            ),
+            BabyStatus(
+                id = 3L,
+                title = "Test 3",
+                date = 3L,
+                height = 47.1F,
+                weight = 2.98F,
+            ),
+            BabyStatus(
+                id = 4L,
+                title = "Test 4",
+                date = 4L,
+                height = 53.3F,
+                weight = 3.23F,
+            ),
+            BabyStatus(
+                id = 5L,
+                title = "Test 5",
+                date = 5L,
+                height = 56.3F,
+                weight = 3.63F,
+            ),
+            BabyStatus(
+                id = 6L,
+                title = "Test 6",
+                date = 6L,
+                height = 57.3F,
+                weight = 3.73F,
+            ),
+            BabyStatus(
+                id = 7L,
+                title = "Test 7",
+                date = 7L,
+                height = 61.5F,
+                weight = 4.1F,
+            ),
+            BabyStatus(
+                id = 8L,
+                title = "Test 8",
+                date = 8L,
+                height = 64.1F,
+                weight = 4.43F,
+            ),
+        )
+
+        adapter.submitList(status)
+
         return view
     }
 
@@ -43,5 +110,10 @@ class HomeFragment : Fragment() {
     private fun navigateToLogin() {
         findNavController().navigate(
             HomeFragmentDirections.actionHomeToLogin())
+    }
+
+    fun navigateToNewBabyStatus() {
+        findNavController().navigate(
+            HomeFragmentDirections.actionHomeToBabyStatusTitleAndDate())
     }
 }
