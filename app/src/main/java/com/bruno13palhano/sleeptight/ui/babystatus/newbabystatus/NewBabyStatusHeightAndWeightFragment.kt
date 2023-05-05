0f538d58de0b3ab1e85bcf321e41160040a2e6f0
bruno13palhano <bruno13palhano@gmail.com>
2023-05-05 19:36:31 -0300

    Handler new BabyStatus fow

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/newbabystatus/NewBabyStatusHeightAndWeightFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/newbabystatus/NewBabyStatusHeightAndWeightFragment.kt
@@ -1,18 +1,75 @@
-package com.bruno13palhano.sleeptight.ui.babystatus
+package com.bruno13palhano.sleeptight.ui.babystatus.newbabystatus
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.bruno13palhano.sleeptight.R
+import com.bruno13palhano.sleeptight.databinding.FragmentNewBabyStatusHeightAndWeightBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class BabyStatusHeightAndWeightFragment : Fragment() {
+@AndroidEntryPoint
+class NewBabyStatusHeightAndWeightFragment : Fragment() {
+    private var _binding: FragmentNewBabyStatusHeightAndWeightBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var inputMethodManager: InputMethodManager
+    private val viewModel: NewBabyStatusViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_baby_status_height_and_weight, container, false)
+    ): View {
+        _binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_new_baby_status_height_and_weight, container, false)
+        val view = binding.root
+
+        binding.uiEvents = this
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        inputMethodManager = activity
+            ?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.height.requestFocus()
+        inputMethodManager.showSoftInput(binding.height, InputMethodManager.SHOW_IMPLICIT)
+        binding.height.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+
+            false
+        }
+
+        binding.weight.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+
+            false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun navigateToHome() {
+        findNavController().navigate(
+            NewBabyStatusHeightAndWeightFragmentDirections.actionBabyStatusHeightAndWeightToHome())
+        viewModel.insertBabyStatus()
     }
 }
--- app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/newbabystatus/NewBabyStatusTitleAndDateFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/newbabystatus/NewBabyStatusTitleAndDateFragment.kt
@@ -1,18 +1,103 @@
-package com.bruno13palhano.sleeptight.ui.babystatus
+package com.bruno13palhano.sleeptight.ui.babystatus.newbabystatus
 
+import android.content.Context
+import android.icu.util.Calendar
+import android.icu.util.TimeZone
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.bruno13palhano.sleeptight.R
+import com.bruno13palhano.sleeptight.databinding.FragmentNewBabyStatusTitleAndDateBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class BabyStatusTitleAndDateFragment : Fragment() {
+@AndroidEntryPoint
+class NewBabyStatusTitleAndDateFragment : Fragment() {
+    private var _binding: FragmentNewBabyStatusTitleAndDateBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var inputMethodManager: InputMethodManager
+    private lateinit var datePicker: MaterialDatePicker<Long>
+    private val viewModel: NewBabyStatusViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_baby_status_title_and_date, container, false)
+    ): View {
+        _binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_new_baby_status_title_and_date, container, false)
+        val view = binding.root
+
+        binding.uiEvents = this
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        inputMethodManager = activity
+            ?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.date.collect {
+                        setDatePicker(it)
+                    }
+                }
+            }
+        }
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.title.requestFocus()
+        inputMethodManager.showSoftInput(binding.title, InputMethodManager.SHOW_IMPLICIT)
+        binding.title.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+
+            false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun navigateToHeightAndWeight() {
+        findNavController().navigate(
+            NewBabyStatusTitleAndDateFragmentDirections.actionBabyStatusFragmentToHeightAndWeight())
+    }
+
+    fun onDateClick() {
+        if (!datePicker.isAdded)
+            datePicker.show(requireParentFragment().parentFragmentManager, "date_dialog")
+    }
+
+    private fun setDatePicker(date: Long) {
+        datePicker = MaterialDatePicker
+            .Builder
+            .datePicker()
+            .setSelection(date)
+            .build()
+        datePicker.addOnPositiveButtonClickListener {
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            calendar.timeInMillis = it
+
+            viewModel.setDate(calendar.timeInMillis)
+        }
     }
 }
