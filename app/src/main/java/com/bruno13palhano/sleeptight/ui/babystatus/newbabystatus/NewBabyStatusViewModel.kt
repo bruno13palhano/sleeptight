88f1a2e30d19c26ea0c74da5743cef1a6692540a
bruno13palhano <bruno13palhano@gmail.com>
2023-05-05 19:34:46 -0300

    Refactored

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/newbabystatus/NewBabyStatusViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/babystatus/newbabystatus/NewBabyStatusViewModel.kt
@@ -10,7 +10,6 @@ import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -21,14 +20,8 @@ class NewBabyStatusViewModel @Inject constructor(
     @DefaultBabyStatusRep private val babyStatusRepository: BabyStatusRepository
 ) : ViewModel() {
 
-    private val _date = MutableStateFlow(MaterialDatePicker.todayInUtcMilliseconds())
-    val date = _date.asStateFlow()
-        .stateIn(
-            scope = viewModelScope,
-            initialValue = 0L,
-            started = WhileSubscribed(5_000)
-        )
-    val dateUi = _date.map {
+    val date = MutableStateFlow(MaterialDatePicker.todayInUtcMilliseconds())
+    val dateUi = date.map {
         DateFormatUtil.format(it)
     }
         .stateIn(
@@ -38,7 +31,7 @@ class NewBabyStatusViewModel @Inject constructor(
         )
 
     fun setDate(dateInMillis: Long) {
-        _date.value = dateInMillis
+        date.value = dateInMillis
     }
 
     val title = MutableStateFlow("")
@@ -46,15 +39,21 @@ class NewBabyStatusViewModel @Inject constructor(
     val weight = MutableStateFlow("")
 
     fun insertBabyStatus() {
-        val babyStatus = BabyStatus(
-            id = 0L,
-            title = title.value,
-            date = date.value,
-            height = height.value.toFloat(),
-            weight = weight.value.toFloat()
-        )
         viewModelScope.launch {
+            val babyStatus = BabyStatus(
+                id = 0L,
+                title = title.value,
+                date = date.value,
+                height = stringToFloat(height.value),
+                weight = stringToFloat(weight.value)
+            )
             babyStatusRepository.insertBabyStatus(babyStatus)
         }
+    }
+
+    private fun stringToFloat(value: String): Float {
+        return try {
+            value.toFloat()
+        } catch (ignored: Exception) { 0F }
     }
 }
