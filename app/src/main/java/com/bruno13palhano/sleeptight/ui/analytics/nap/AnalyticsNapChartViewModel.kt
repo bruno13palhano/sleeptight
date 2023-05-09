6e3d50026f6cf893df0d252e4459b72d1e857b02
bruno13palhano <bruno13palhano@gmail.com>
2023-05-09 15:09:12 -0300

    Added a viewmodel

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/analytics/nap/AnalyticsNapChartViewModel.kt
@@ -0,0 +1,59 @@
+package com.bruno13palhano.sleeptight.ui.analytics.nap
+
+import android.icu.util.Calendar
+import android.icu.util.TimeZone
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.di.DefaultNapRep
+import com.bruno13palhano.core.data.repository.NapRepository
+import com.bruno13palhano.sleeptight.ui.util.DateFormatUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class AnalyticsNapChartViewModel @Inject constructor(
+    @DefaultNapRep napRepository: NapRepository
+) : ViewModel() {
+
+    val napChartUi = napRepository.getAllStream()
+        .map {
+            val allSleepTime = mutableListOf<Float>()
+            val allDate = mutableListOf<String>()
+
+            it.forEach { nap ->
+                allSleepTime.add(timeToDecimal(nap.sleepTime))
+                allDate.add(DateFormatUtil.format(nap.date))
+            }
+
+            NapChartUi(
+                allSleeptime = allSleepTime,
+                allDate = allDate
+            )
+
+        }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = NapChartUi(),
+            started = WhileSubscribed(5_000)
+        )
+
+    data class NapChartUi(
+        val allSleeptime: List<Float> = emptyList(),
+        val allDate: List<String> = emptyList()
+    )
+
+    private fun timeToDecimal(time: Long): Float {
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        calendar.timeInMillis = time
+
+        val hour = calendar[Calendar.HOUR_OF_DAY]
+        val minute = calendar[Calendar.MINUTE]
+
+        val timeDecimal = "$hour.${minute * 100 / 60}".toFloat()
+        return if (timeDecimal == 0.0F) 0.0F else String.format("%.2f", timeDecimal)
+            .replace(",", ".").toFloat()
+    }
+}
