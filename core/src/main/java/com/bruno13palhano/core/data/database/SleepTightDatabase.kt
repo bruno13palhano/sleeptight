aed7c1fc305f595e5b891aabb663d01d99e08cb0
bruno13palhano <bruno13palhano@gmail.com>
2023-05-12 19:55:00 -0300

    Added NotificationDao and NotificationData to SleepTightDatabase

--- core/src/main/java/com/bruno13palhano/core/data/database/SleepTightDatabase.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/SleepTightDatabase.kt
@@ -4,16 +4,19 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.bruno13palhano.core.data.database.dao.BabyStatusDao
 import com.bruno13palhano.core.data.database.dao.NapDao
+import com.bruno13palhano.core.data.database.dao.NotificationDao
 import com.bruno13palhano.core.data.database.dao.UserDao
 import com.bruno13palhano.core.data.database.model.BabyStatusData
 import com.bruno13palhano.core.data.database.model.NapData
+import com.bruno13palhano.core.data.database.model.NotificationData
 import com.bruno13palhano.core.data.database.model.UserData
 
 @Database(
     entities = [
         NapData::class,
         UserData::class,
-        BabyStatusData::class
+        BabyStatusData::class,
+        NotificationData::class
     ],
     version = 1,
     exportSchema = false
@@ -22,4 +25,5 @@ internal abstract class SleepTightDatabase : RoomDatabase() {
     abstract val napDao: NapDao
     abstract val userDao: UserDao
     abstract val babyStatusDao: BabyStatusDao
+    abstract val notificationDao: NotificationDao
 }
