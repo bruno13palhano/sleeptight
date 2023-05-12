6f7e3aba32d8d60907ea55bd8daf482a24fcc720
bruno13palhano <bruno13palhano@gmail.com>
2023-05-12 14:49:31 -0300

    Created fragments for notification flow

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/notificaitons/NotificationsFragment.kt
@@ -0,0 +1,31 @@
+package com.bruno13palhano.sleeptight.ui.notificaitons
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.bruno13palhano.sleeptight.R
+import com.bruno13palhano.sleeptight.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+    private var _binding: FragmentNotificationsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_notifications, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/notificaitons/newnotification/NewNotificationFragment.kt
@@ -0,0 +1,31 @@
+package com.bruno13palhano.sleeptight.ui.notificaitons.newnotification
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.bruno13palhano.sleeptight.R
+import com.bruno13palhano.sleeptight.databinding.FragmentNewNotificationBinding
+
+class NewNotificationFragment : Fragment() {
+    private var _binding: FragmentNewNotificationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil
+            .inflate(inflater, R.layout.fragment_new_notification, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
