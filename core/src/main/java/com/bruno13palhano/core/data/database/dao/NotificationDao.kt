14c72dda56a381d57b7468051099c624415a7bf5
bruno13palhano <bruno13palhano@gmail.com>
2023-05-12 19:51:49 -0300

    Created NotificationDao

---
+++ core/src/main/java/com/bruno13palhano/core/data/database/dao/NotificationDao.kt
@@ -0,0 +1,27 @@
+package com.bruno13palhano.core.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.bruno13palhano.core.data.database.model.NotificationData
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+internal interface NotificationDao {
+
+    @Query("SELECT * FROM notification_table")
+    fun getAllNotificationsStream(): Flow<List<NotificationData>>
+
+    @Query("SELECT * FROM notification_table WHERE id = :id")
+    fun getNotificationByIdStream(id: Long): Flow<NotificationData>
+
+    @Insert
+    suspend fun insertNotification(notification: NotificationData)
+
+    @Query("DELETE FROM notification_table WHERE id = :id")
+    suspend fun deleteNotificationById(id: Long)
+
+    @Update
+    suspend fun updateNotification(notification: NotificationData)
+}
