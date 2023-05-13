93d0813bb518d77d30ffd1f164f1f25d2f4c679e
bruno13palhano <bruno13palhano@gmail.com>
2023-05-13 20:43:08 -0300

    Created a ViewModel

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/notifications/NotificationFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/notifications/NotificationFragment.kt
@@ -6,12 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentNotificationBinding
+import com.bruno13palhano.sleeptight.ui.util.TimePickerUtil
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class NotificationFragment : Fragment() {
     private var _binding: FragmentNotificationBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: NotificationViewModel by viewModels()
+    private var notificationId: Long = 0L
+
+    private lateinit var datePicker: MaterialDatePicker<Long>
+    private lateinit var timePicker: MaterialTimePicker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,11 +37,69 @@ class NotificationFragment : Fragment() {
             .inflate(inflater, R.layout.fragment_notification, container, false)
         val view = binding.root
 
+        binding.uiEvents = this
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        notificationId = NotificationFragmentArgs.fromBundle(requireArguments()).notificationId
+
+        viewModel.setNotification(notificationId)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.hour.collect {
+                        setTimepicker(it)
+                    }
+                }
+                launch {
+                    viewModel.date.collect {
+                        setDatePicker(it)
+                    }
+                }
+            }
+        }
+
         return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun updateNotification() {
+        findNavController().navigateUp()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.updateNotification(notificationId)
+        }
+    }
+
+    fun onHourClick() {
+        if (!timePicker.isAdded)
+            timePicker.show(requireParentFragment().parentFragmentManager, "hour_dialog")
+    }
+
+    fun onDateClick() {
+        if (!datePicker.isAdded)
+            datePicker.show(requireParentFragment().parentFragmentManager, "date_dialog")
+    }
+
+    private fun setDatePicker(date: Long) {
+        datePicker = MaterialDatePicker
+            .Builder
+            .datePicker()
+            .setSelection(date)
+            .build()
+        datePicker.addOnPositiveButtonClickListener {
+            viewModel.setDate(it)
+        }
+    }
+
+    private fun setTimepicker(time: Long) {
+        timePicker = TimePickerUtil.prepareTimePicker(time)
+        timePicker.addOnPositiveButtonClickListener {
+            viewModel.setHour(timePicker.hour, timePicker.minute)
+        }
     }
 }
---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/notifications/NotificationViewModel.kt
@@ -0,0 +1,98 @@
+package com.bruno13palhano.sleeptight.ui.lists.notifications
+
+import android.icu.text.DateFormat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.di.DefaultNotificationRep
+import com.bruno13palhano.core.data.repository.NotificationRepository
+import com.bruno13palhano.model.Notification
+import com.bruno13palhano.sleeptight.ui.util.CalendarUtil
+import com.bruno13palhano.sleeptight.ui.util.DateFormatUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NotificationViewModel @Inject constructor(
+    @DefaultNotificationRep private val notificationRepository: NotificationRepository
+) : ViewModel() {
+
+    val title = MutableStateFlow("")
+    val description = MutableStateFlow("")
+    val repeat = MutableStateFlow(false)
+
+    private val _hour = MutableStateFlow(0L)
+    val hour = _hour.asStateFlow()
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = 0L,
+            started = WhileSubscribed(5_000)
+        )
+
+    val hourUi = _hour.asStateFlow()
+        .map {
+            DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(it)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = "",
+            started = WhileSubscribed(5_000)
+        )
+
+    fun setHour(hour: Int, minute: Int) {
+        _hour.value = CalendarUtil.timeToMilliseconds(hour, minute)
+    }
+
+    private val _date = MutableStateFlow(0L)
+    val date = _date.asStateFlow()
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = 0L,
+            started = WhileSubscribed(5_000)
+        )
+
+    val dateUi = _date.asStateFlow()
+        .map {
+            DateFormatUtil.format(it)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = "",
+            started = WhileSubscribed(5_000)
+        )
+
+    fun setDate(date: Long) {
+        _date.value = date
+    }
+
+    fun setNotification(id: Long) {
+        viewModelScope.launch {
+            notificationRepository.getNotificationByIdStream(id).collect {
+                title.value = it.title
+                description.value = it.description
+                repeat.value = it.repeat
+                _hour.value = it.time
+                _date.value = it.date
+            }
+        }
+    }
+
+    fun updateNotification(id: Long) {
+        val notification = Notification(
+            id = id,
+            title = title.value,
+            description = description.value,
+            repeat = repeat.value,
+            time = _hour.value,
+            date = _date.value
+        )
+        viewModelScope.launch {
+            notificationRepository.updateNotification(notification)
+        }
+    }
+}
