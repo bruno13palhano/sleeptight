cf780f64edeecdb8b286e208b0ad61e53dfd4d1f
bruno13palhano <bruno13palhano@gmail.com>
2023-05-17 22:15:54 -0300

    Refactored, AlarmNotification wrapper added

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/notifications/work/NotificationWork.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/notifications/work/NotificationWork.kt
@@ -1,26 +1,21 @@
 package com.bruno13palhano.sleeptight.ui.lists.notifications.work
 
 import android.app.AlarmManager
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Context.ALARM_SERVICE
 import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
-import android.icu.util.Calendar
-import android.icu.util.TimeZone
-import android.os.Build
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.bruno13palhano.core.data.di.DefaultNotificationRep
 import com.bruno13palhano.core.data.repository.NotificationRepository
+import com.bruno13palhano.sleeptight.ui.lists.notifications.AlarmNotification
 import com.bruno13palhano.sleeptight.ui.lists.notifications.receivers.NotificationReceiver
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-
-private const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
 
 @HiltWorker
 class NotificationWork @AssistedInject constructor(
@@ -54,50 +49,21 @@ class NotificationWork @AssistedInject constructor(
 
                     val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
 
-                    alarmManager.setRepeating(
-                        AlarmManager.RTC_WAKEUP,
-                        getNotificationDateAndTime(notification.time, notification.date),
-                        AlarmManager.INTERVAL_FIFTEEN_MINUTES,
-                        notifyPendingIntent
+                    val alarmNotification = AlarmNotification(
+                        notificationManager = notificationManager,
+                        alarmManager = alarmManager
                     )
 
-                    createNotificationChannel(context)
+                    alarmNotification.setAlarmManager(
+                        notifyPendingIntent = notifyPendingIntent,
+                        time = notification.time,
+                        date = notification.date,
+                        repeat = true
+                    )
                 }
             }
         }
 
         return Result.success()
-    }
-
-    private fun createNotificationChannel(context: Context) {
-        notificationManager =
-            context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                PRIMARY_CHANNEL_ID, "SleepTight notification", NotificationManager.IMPORTANCE_HIGH)
-
-            notificationChannel.enableLights(true)
-            notificationChannel.enableVibration(true)
-            notificationChannel.description = "Notifies user for test"
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-    }
-
-    private fun getNotificationDateAndTime(time: Long, date: Long): Long {
-        val calendarTime = Calendar.getInstance()
-        val calendarDate = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-        calendarTime.timeInMillis = time
-        calendarDate.timeInMillis = date
-
-        val calendar = Calendar.getInstance()
-        calendar[Calendar.HOUR_OF_DAY] = calendarTime[Calendar.HOUR_OF_DAY]
-        calendar[Calendar.MINUTE] = calendarTime[Calendar.MINUTE]
-        calendar[Calendar.SECOND] = 0
-        calendar[Calendar.YEAR] = calendarDate[Calendar.YEAR]
-        calendar[Calendar.MONTH] = calendarDate[Calendar.MONTH]
-        calendar[Calendar.DAY_OF_MONTH] = calendarDate[Calendar.DAY_OF_MONTH]
-
-        return calendar.timeInMillis
     }
 }
