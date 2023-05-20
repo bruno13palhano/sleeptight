01c8775685519dc2cec3f44bd713c5fcdebcbe8f
bruno13palhano <bruno13palhano@gmail.com>
2023-05-20 19:00:58 -0300

    Added ui state

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/home/HomeViewModel.kt
@@ -5,24 +5,70 @@ import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.authentication.DefaultUserFirebase
 import com.bruno13palhano.authentication.UserAuthentication
 import com.bruno13palhano.core.data.di.DefaultBabyStatusRep
+import com.bruno13palhano.core.data.di.DefaultUserRep
 import com.bruno13palhano.core.data.repository.BabyStatusRepository
+import com.bruno13palhano.core.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     @DefaultUserFirebase private val authentication: UserAuthentication,
+    @DefaultUserRep private val userRepository: UserRepository,
     @DefaultBabyStatusRep private val babyStatusRepository: BabyStatusRepository
 ) : ViewModel() {
-
-    val allBabyStatus = babyStatusRepository.getAllStream()
+    private val _babyName = MutableStateFlow("")
+    val babyName = _babyName.asStateFlow()
         .stateIn(
             scope = viewModelScope,
-            initialValue = emptyList(),
+            initialValue = "",
             started = WhileSubscribed(5_000)
         )
+
+    private val _profileImage = MutableStateFlow("")
+    val profileImage = _profileImage.asStateFlow()
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = "",
+            started = WhileSubscribed(5_000)
+        )
+
+    private val _height = MutableStateFlow(0F)
+    val height = _height.asStateFlow()
+        .map { "$it cm" }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = "",
+            started = WhileSubscribed(5_000)
+        )
+
+    private val _weight = MutableStateFlow(0F)
+    val weight = _weight.asStateFlow()
+        .map { "$it kg" }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = "",
+            started = WhileSubscribed(5_000)
+        )
+
+    init {
+        viewModelScope.launch {
+            userRepository.getUserByIdStream(authentication.getCurrentUser().id).collect {
+                _babyName.value = it.babyName
+                _profileImage.value = it.babyUrlPhoto
+            }
+        }
+
+        viewModelScope.launch {
+            babyStatusRepository.getLastBabyStatusStream().collect {
+                _height.value = it.height
+                _weight.value = it.weight
+            }
+        }
+    }
 
     fun isUserAuthenticated(): Boolean =
         authentication.isUserAuthenticated()
