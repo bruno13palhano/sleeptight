0fa7306bd482e7f855e0576cb1508d1d6eafb41e
bruno13palhano <bruno13palhano@gmail.com>
2023-05-25 23:31:29 -0300

    Renamed interface and add a method to get the last model

--- core/src/main/java/com/bruno13palhano/core/data/repository/CommonRepository.kt
+++ core/src/main/java/com/bruno13palhano/core/data/repository/CommonRepository.kt
@@ -2,10 +2,11 @@ package com.bruno13palhano.core.data.repository
 
 import kotlinx.coroutines.flow.Flow
 
-interface RepositoryCrud<T> {
+interface CommonRepository<T> {
     suspend fun insert(model: T)
     suspend fun update(model: T)
     suspend fun deleteById(id: Long)
     fun getByIdStream(id: Long): Flow<T>
     fun getAllStream(): Flow<List<T>>
+    fun getLastStream(): Flow<T>
 }
