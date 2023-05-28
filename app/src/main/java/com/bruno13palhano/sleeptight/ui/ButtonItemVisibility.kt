0f0c09ab9875aed23b88babaa6bb87e0149eec59
bruno13palhano <bruno13palhano@gmail.com>
2023-05-28 20:42:22 -0300

    Removed a method and moved to ui package

---
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/ButtonItemVisibility.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.sleeptight.ui
+
+interface ButtonItemVisibility {
+    fun enableButton()
+    fun disableButton()
+}
--- app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/ButtonItemVisibility.kt
+++
@@ -1,7 +0,0 @@
-package com.bruno13palhano.sleeptight.ui.lists
-
-interface ButtonItemVisibility {
-    fun setButtonVisibility(title: String)
-    fun enableButton()
-    fun disableButton()
-}
