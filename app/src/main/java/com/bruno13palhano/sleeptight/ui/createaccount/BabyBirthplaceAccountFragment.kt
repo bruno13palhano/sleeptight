3e44ed03e6b5d549272b06937c0ad7515184543c
bruno13palhano <bruno13palhano@gmail.com>
2023-05-29 18:42:10 -0300

    Show button only when paramters are not empty

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/createaccount/BabyBirthplaceAccountFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/createaccount/BabyBirthplaceAccountFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentBabyBirthplaceAccountBinding
+import com.bruno13palhano.sleeptight.ui.ButtonItemVisibility
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class BabyBirthplaceAccountFragment : Fragment() {
+class BabyBirthplaceAccountFragment : Fragment(), ButtonItemVisibility {
     private var _binding: FragmentBabyBirthplaceAccountBinding? = null
     private val binding get() = _binding!!
     private val viewModel: CreateAccountViewModel by activityViewModels()
@@ -30,6 +37,14 @@ class BabyBirthplaceAccountFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.birthplace.collect {
+                    setButtonVisibility(it)
+                }
+            }
+        }
+
         return view
     }
 
@@ -41,5 +56,21 @@ class BabyBirthplaceAccountFragment : Fragment() {
     fun navigateToBirth() {
         findNavController().navigate(
             BabyBirthplaceAccountFragmentDirections.actionBabyBirthplaceToBabyBirth())
+    }
+
+    private fun setButtonVisibility(birthplace: String) {
+        if (birthplace.trim() != "") {
+            enableButton()
+        } else {
+            disableButton()
+        }
+    }
+
+    override fun enableButton() {
+        binding.next.visibility = VISIBLE
+    }
+
+    override fun disableButton() {
+        binding.next.visibility = GONE
     }
 }
--- app/src/main/java/com/bruno13palhano/sleeptight/ui/createaccount/BabyNameAccountFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/createaccount/BabyNameAccountFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentBabyNameAccountBinding
+import com.bruno13palhano.sleeptight.ui.ButtonItemVisibility
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class BabyNameAccountFragment : Fragment() {
+class BabyNameAccountFragment : Fragment(), ButtonItemVisibility {
     private var _binding: FragmentBabyNameAccountBinding? = null
     private val binding get() = _binding!!
     private val viewModel: CreateAccountViewModel by activityViewModels()
@@ -30,6 +37,14 @@ class BabyNameAccountFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.babyName.collect {
+                    setButtonVisibility(it)
+                }
+            }
+        }
+
         return view
     }
 
@@ -41,5 +56,21 @@ class BabyNameAccountFragment : Fragment() {
     fun navigateToBirthplace() {
         findNavController().navigate(
             BabyNameAccountFragmentDirections.actionBabyNameToBayBirthplace())
+    }
+
+    private fun setButtonVisibility(name: String) {
+        if (name.trim() != "") {
+            enableButton()
+        } else {
+            disableButton()
+        }
+    }
+
+    override fun enableButton() {
+        binding.next.visibility = VISIBLE
+    }
+
+    override fun disableButton() {
+        binding.next.visibility = GONE
     }
 }
