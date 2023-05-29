2d2b9518c03f60d6d5769135c53c71f8a128d0c5
bruno13palhano <bruno13palhano@gmail.com>
2023-05-29 18:03:03 -0300

    Show button only when paramters are not empty

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/createaccount/CreateAccountFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/createaccount/CreateAccountFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.databinding.FragmentCreateAccountBinding
+import com.bruno13palhano.sleeptight.ui.ButtonItemVisibility
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class CreateAccountFragment : Fragment() {
+class CreateAccountFragment : Fragment(), ButtonItemVisibility {
     private var _binding: FragmentCreateAccountBinding? = null
     private val binding get() = _binding!!
     private val viewModel: CreateAccountViewModel by activityViewModels()
@@ -30,6 +37,20 @@ class CreateAccountFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.createAccountUi.collect {
+                        setButtonVisibility(
+                            username = it.username,
+                            email = it.email,
+                            password = it.password
+                        )
+                    }
+                }
+            }
+        }
+
         return view
     }
 
@@ -45,5 +66,25 @@ class CreateAccountFragment : Fragment() {
 
     fun navigateToLogin() {
         findNavController().navigateUp()
+    }
+
+    private fun setButtonVisibility(
+        username: String,
+        email: String,
+        password: String
+    ) {
+        if (username.trim() != "" && email.trim() != "" && password.trim() != "") {
+            enableButton()
+        } else {
+            disableButton()
+        }
+    }
+
+    override fun enableButton() {
+        binding.next.visibility = VISIBLE
+    }
+
+    override fun disableButton() {
+        binding.next.visibility = GONE
     }
 }
