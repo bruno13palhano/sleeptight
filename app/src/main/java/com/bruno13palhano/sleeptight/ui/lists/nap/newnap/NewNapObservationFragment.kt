eb947f619c0fbc7d0b2bf0b07bf2f44c74158fd5
bruno13palhano <bruno13palhano@gmail.com>
2023-05-29 21:09:49 -0300

    Navigate to date when observarion imeAction is equals done

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/nap/newnap/NewNapObservationFragment.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/lists/nap/newnap/NewNapObservationFragment.kt
@@ -1,5 +1,6 @@
 package com.bruno13palhano.sleeptight.ui.lists.nap.newnap
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -22,6 +25,8 @@ class NewNapObservationFragment : Fragment(), ButtonItemVisibility {
     private var _binding: FragmentNewNapObservationBinding? = null
     private val binding get() = _binding!!
     private val viewModel: NewNapViewModel by activityViewModels()
+    private lateinit var inputMethodManager: InputMethodManager
+    private var title = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,16 +40,48 @@ class NewNapObservationFragment : Fragment(), ButtonItemVisibility {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
 
+        inputMethodManager = activity
+            ?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.title.collect {
                     setButtonVisibility(it)
+                    title = it
                 }
             }
         }
 
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.newNapTitle.requestFocus()
+        inputMethodManager.showSoftInput(binding.newNapTitle, InputMethodManager.SHOW_IMPLICIT)
+        binding.newNapTitle.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                binding.newNapObservation.requestFocus()
+            }
+
+            false
+        }
+
+        binding.newNapObservation.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+                if (isTitleNotEmpty()) {
+                    navigateToDate()
+                }
+            }
+
+            false
+        }
+    }
+
+    private fun isTitleNotEmpty()
+        = title != ""
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -53,8 +90,7 @@ class NewNapObservationFragment : Fragment(), ButtonItemVisibility {
 
     fun navigateToDate() {
         findNavController().navigate(
-            NewNapObservationFragmentDirections.actionObservationToDate()
-        )
+            NewNapObservationFragmentDirections.actionObservationToDate())
     }
 
     private fun setButtonVisibility(title: String) {
