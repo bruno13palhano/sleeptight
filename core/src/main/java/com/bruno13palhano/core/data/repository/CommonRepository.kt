557b8af149e36eeac7da7aa55c055d2f2424ed0f
bruno13palhano <bruno13palhano@gmail.com>
2023-05-31 11:45:49 -0300

    Insert method now return the item id

--- core/src/main/java/com/bruno13palhano/core/data/repository/CommonRepository.kt
+++ core/src/main/java/com/bruno13palhano/core/data/repository/CommonRepository.kt
@@ -3,7 +3,7 @@ package com.bruno13palhano.core.data.repository
 import kotlinx.coroutines.flow.Flow
 
 interface CommonRepository<T> {
-    suspend fun insert(model: T)
+    suspend fun insert(model: T): Long
     suspend fun update(model: T)
     suspend fun deleteById(id: Long)
     fun getByIdStream(id: Long): Flow<T>
