479a447c88d47255950acf8c6148d7ff2d2edf19
bruno13palhano <bruno13palhano@gmail.com>
2023-06-13 19:38:18 -0300

    Add navigation actions to Notification flow

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/NotificationsNavigationActions.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/NotificationsNavigationActions.kt
@@ -1,11 +1,23 @@
 package com.bruno13palhano.sleeptight.ui.navigation
 
-class NotificationsNavigationActions {
+import androidx.navigation.NavController
 
+class NotificationsNavigationActions(navController: NavController) {
+    val navigateFromNotificationsToNotification: (notificationId: Long) -> Unit = { notificationId ->
+        navController.navigate("${NotificationsDestinations.NOTIFICATION_ROUTE}$notificationId") {
+            popUpTo(NotificationsDestinations.NOTIFICATIONS_ROUTE)
+        }
+    }
+    val navigateFromNotificationsToNewNotification: () -> Unit = {
+        navController.navigate(NotificationsDestinations.NEW_NOTIFICATION_ROUTE) {
+            popUpTo(NotificationsDestinations.NOTIFICATIONS_ROUTE)
+        }
+    }
 }
 
 object NotificationsDestinations {
     const val NOTIFICATIONS_ROUTE = "notifications"
-    const val NOTIFICATION_ROUTE = "notification"
+    const val NOTIFICATION_ROUTE = "notification/"
+    const val NOTIFICATION_WITH_ID_ROUTE = "$NOTIFICATION_ROUTE{notificationId}"
     const val NEW_NOTIFICATION_ROUTE = "new_notification"
 }
