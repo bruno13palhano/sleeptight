0ccb5fc802cfe6851840bd23052d80a7c38bdcb2
bruno13palhano <bruno13palhano@gmail.com>
2023-06-13 11:32:52 -0300

    Add navigation actions

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/ListsNavigationActions.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/ListsNavigationActions.kt
@@ -1,11 +1,28 @@
 package com.bruno13palhano.sleeptight.ui.navigation
 
-class ListsNavigationActions {
+import androidx.navigation.NavController
+
+class ListsNavigationActions(navController: NavController) {
+    val navigateToBabyStatusList: () -> Unit = {
+        navController.navigate(ListsDestinations.BABY_STATUS_LIST_ROUTE) {
+            popUpTo(ListsDestinations.ALL_LISTS_ROUTE)
+        }
+    }
+    val navigateToNapsList: () -> Unit = {
+        navController.navigate(ListsDestinations.NAP_LIST_ROUTE) {
+            popUpTo(ListsDestinations.ALL_LISTS_ROUTE)
+        }
+    }
+    val navigateToNotificationsList: () -> Unit = {
+        navController.navigate(ListsDestinations.NOTIFICATIONS_LIST_ROUTE) {
+            popUpTo(ListsDestinations.ALL_LISTS_ROUTE)
+        }
+    }
 }
 
 object ListsDestinations {
     const val ALL_LISTS_ROUTE = "all_lists"
-    const val LISTS_BABY_STATUS_ROUTE = "lists_to_baby_status"
-    const val LISTS_NAPS_ROUTE = "lists_to_naps"
-    const val LISTS_NOTIFICATIONS_ROUTE = "lists_to_notifications"
+    const val BABY_STATUS_LIST_ROUTE = "lists_to_baby_status"
+    const val NAP_LIST_ROUTE = "lists_to_naps"
+    const val NOTIFICATIONS_LIST_ROUTE = "lists_to_notifications"
 }
