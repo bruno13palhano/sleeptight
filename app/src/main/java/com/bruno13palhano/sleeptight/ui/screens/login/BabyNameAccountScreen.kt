012ac368dc9ea363981f6332cf08e1690cf27390
bruno13palhano <bruno13palhano@gmail.com>
2023-06-15 12:58:15 -0300

    Add BabyNameAccountScreen content

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyNameAccountScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyNameAccountScreen.kt
@@ -1,19 +1,116 @@
 package com.bruno13palhano.sleeptight.ui.screens.login
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Label
+import androidx.compose.material.icons.filled.NavigateNext
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bruno13palhano.sleeptight.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BabyNameAccountScreen(
     onNextButtonClick: () -> Unit
 ) {
-    Column {
-        Text(text = "Baby Name Account Screen")
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = onNextButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.NavigateNext,
+                    contentDescription = stringResource(id = R.string.next_label)
+                )
+            }
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            val focusManager = LocalFocusManager.current
+            var babyName by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, start = 16.dp, end = 16.dp),
+                value = babyName,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Label,
+                        contentDescription = stringResource(id = R.string.baby_name_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    this.defaultKeyboardAction(ImeAction.Done)
+                    focusManager.clearFocus()
+                }),
+                onValueChange = { babyNameValue ->
+                    babyName = babyNameValue
+                },
+                singleLine = true,
+                label = { Text(text = stringResource(id = R.string.baby_name_label)) },
+                placeholder = { Text(text = stringResource(id = R.string.insert_baby_name_label)) }
+            )
+        }
+    }
+}
 
-        Button(onClick = onNextButtonClick) {
-            Text(text = "Go to Baby Birthplace")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun BabyNameAccountScreenPreview() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.NavigateNext,
+                    contentDescription = stringResource(id = R.string.next_label)
+                )
+            }
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            var babyName by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, start = 16.dp, end = 16.dp),
+                value = babyName,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Label,
+                        contentDescription = stringResource(id = R.string.baby_name_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+
+                }),
+                onValueChange = { babyNameValue ->
+                    babyName = babyNameValue
+                },
+                singleLine = true,
+                label = { Text(text = stringResource(id = R.string.baby_name_label)) },
+                placeholder = { Text(text = stringResource(id = R.string.insert_baby_name_label)) }
+            )
         }
     }
 }
