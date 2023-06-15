e3d7c1b50cb1457d282de11bb187b147d836fab5
bruno13palhano <bruno13palhano@gmail.com>
2023-06-15 14:49:27 -0300

    Add BabyBirthplaceAccountScreen content

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyBirthplaceAccountScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyBirthplaceAccountScreen.kt
@@ -1,19 +1,81 @@
 package com.bruno13palhano.sleeptight.ui.screens.login
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NavigateNext
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bruno13palhano.sleeptight.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BabyBirthplaceAccountScreen(
     onNextButtonClick: () -> Unit
 ) {
-    Column {
-        Text(text = "Baby Birthplace Account Screen")
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = onNextButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.NavigateNext,
+                    contentDescription = stringResource(id = R.string.next_label)
+                )
+            }
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            val focusManager = LocalFocusManager.current
+            var birthplace by remember { mutableStateOf(TextFieldValue("")) }
+            BirthplaceField(
+                birthplace = birthplace,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, start = 16.dp, end = 16.dp),
+                onBirthplaceChange = { birthplaceValue -> birthplace = birthplaceValue },
+                onDone = { focusManager.clearFocus() }
+            )
+        }
+    }
+}
 
-        Button(onClick = onNextButtonClick) {
-            Text(text = "Go to Baby Birth")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun BabyBirthplaceAccountScreenPreview() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.NavigateNext,
+                    contentDescription = stringResource(id = R.string.next_label)
+                )
+            }
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            var birthplace by remember { mutableStateOf(TextFieldValue("")) }
+            BirthplaceField(
+                birthplace = birthplace,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, start = 16.dp, end = 16.dp),
+                onBirthplaceChange = { birthplaceValue -> birthplace = birthplaceValue },
+                onDone = {}
+            )
         }
     }
 }
