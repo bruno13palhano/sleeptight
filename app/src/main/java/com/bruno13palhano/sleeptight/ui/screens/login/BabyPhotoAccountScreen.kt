98939cc17736a093e8b4f7b4b36d2f2aced219ec
bruno13palhano <bruno13palhano@gmail.com>
2023-06-15 23:09:20 -0300

    Add BabyPhotoAccountScreen content

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyPhotoAccountScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyPhotoAccountScreen.kt
@@ -1,19 +1,88 @@
 package com.bruno13palhano.sleeptight.ui.screens.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.NavigateNext
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bruno13palhano.sleeptight.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BabyPhotoAccountScreen(
     onNextButtonClick: () -> Unit
 ) {
-    Column {
-        Text(text = "Baby Photo Account Screen")
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = onNextButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.NavigateNext,
+                    contentDescription = stringResource(id = R.string.next_label)
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(it)
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(top = 32.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .clip(CircleShape),
+                imageVector = Icons.Filled.Image,
+                contentDescription = stringResource(id = R.string.baby_photo_label)
+            )
+        }
+    }
+}
 
-        Button(onClick = onNextButtonClick) {
-            Text(text = "Go to Baby Name")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun BabyPhotoAccountScreen() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.NavigateNext,
+                    contentDescription = stringResource(id = R.string.next_label)
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(it)
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(top = 32.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .clip(CircleShape),
+                imageVector = Icons.Filled.Image,
+                contentDescription = stringResource(id = R.string.baby_photo_label)
+            )
         }
     }
 }
