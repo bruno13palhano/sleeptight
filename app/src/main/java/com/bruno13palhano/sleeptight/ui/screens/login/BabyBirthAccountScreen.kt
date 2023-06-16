07fc634118ed1fbc5af10a9303b01c0194233b97
bruno13palhano <bruno13palhano@gmail.com>
2023-06-15 23:11:37 -0300

    Add BabyBirthAccountScreen content and handle screen orientation

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyBirthAccountScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyBirthAccountScreen.kt
@@ -1,25 +1,44 @@
 package com.bruno13palhano.sleeptight.ui.screens.login
 
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bruno13palhano.sleeptight.R
@@ -29,61 +48,123 @@ import com.bruno13palhano.sleeptight.R
 fun BabyBirthAccountScreen(
     onCreateAccountSuccess: () -> Unit
 ) {
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = onCreateAccountSuccess) {
-                Icon(
-                    imageVector = Icons.Filled.Done,
-                    contentDescription = stringResource(id = R.string.done_label)
-                )
+    val configuration = LocalConfiguration.current
+    val focusManager = LocalFocusManager.current
+    var date by remember { mutableStateOf(TextFieldValue("")) }
+    var time by remember { mutableStateOf(TextFieldValue("")) }
+    var height by remember { mutableStateOf(TextFieldValue("")) }
+    var weight by remember { mutableStateOf(TextFieldValue("")) }
+
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(onClick = onCreateAccountSuccess) {
+                        Icon(
+                            imageVector = Icons.Filled.Done,
+                            contentDescription = stringResource(id = R.string.done_label)
+                        )
+                    }
+                }
+            ) {
+                Column(modifier = Modifier.padding(it)) {
+                    CommonFields(
+                        date = date,
+                        time = time,
+                        height = height,
+                        weight = weight,
+                        focusManager = focusManager,
+                        onDateClick = {},
+                        onTimeClick = {},
+                        onHeightChange = { heightValue -> height = heightValue },
+                        onWeightChange = { weightValue -> weight = weightValue }
+                    )
+                }
             }
         }
-    ) {
-        Column(modifier = Modifier.padding(it)) {
-            var date by remember { mutableStateOf(TextFieldValue("")) }
-            DateField(
+        else -> {
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 32.dp, start = 16.dp, end = 16.dp)
-                    .clickable { println("Date was clicked!") },
-                date = date
-            )
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+            ) {
+                CommonFields(
+                    date = date,
+                    time = time,
+                    height = height,
+                    weight = weight,
+                    focusManager = focusManager,
+                    onDateClick = {},
+                    onTimeClick = {},
+                    onHeightChange = { heightValue -> height = heightValue },
+                    onWeightChange = { weightValue -> weight = weightValue }
+                )
 
-            var time by remember { mutableStateOf(TextFieldValue("")) }
-            TimeField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 16.dp, end = 16.dp)
-                    .clickable { println("Time was clicked!") },
-                time = time
-            )
-
-            val focusManager = LocalFocusManager.current
-            var height by remember { mutableStateOf(TextFieldValue("")) }
-            HeightField(
-                height = height,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
-                onHeightChange = { heightValue -> height = heightValue },
-                onDone = { focusManager.moveFocus(FocusDirection.Next) }
-            )
-
-            var weight by remember { mutableStateOf(TextFieldValue("")) }
-            WeightField(
-                weight = weight,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
-                onWeightChange = { weightValue -> weight = weightValue },
-                onDone = { focusManager.clearFocus() },
-            )
+                FloatingActionButton(
+                    onClick = onCreateAccountSuccess,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.End)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Done,
+                        contentDescription = stringResource(id = R.string.done_label)
+                    )
+                }
+            }
         }
     }
 }
 
+@Composable
+private fun CommonFields(
+    date: TextFieldValue,
+    time: TextFieldValue,
+    height: TextFieldValue,
+    weight: TextFieldValue,
+    focusManager: FocusManager,
+    onDateClick: () -> Unit,
+    onTimeClick: () -> Unit,
+    onHeightChange: (height: TextFieldValue) -> Unit,
+    onWeightChange: (weight: TextFieldValue) -> Unit
+) {
+    DateField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+            .clickable { onDateClick() },
+        date = date
+    )
+
+    TimeField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, start = 16.dp, end = 16.dp)
+            .clickable { onTimeClick() },
+        time = time
+    )
+
+    HeightField(
+        height = height,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, start = 16.dp, end = 16.dp),
+        onHeightChange = { heightValue -> onHeightChange(heightValue) },
+        onDone = { focusManager.moveFocus(FocusDirection.Next) }
+    )
+
+    WeightField(
+        weight = weight,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, start = 16.dp, end = 16.dp),
+        onWeightChange = { weightValue -> onWeightChange(weightValue) },
+        onDone = { focusManager.clearFocus() },
+    )
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun BabyBirthAccountScreenPreview() {
     Scaffold(
@@ -97,36 +178,16 @@ fun BabyBirthAccountScreenPreview() {
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
-            DateField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 32.dp, start = 16.dp, end = 16.dp),
-                date = TextFieldValue("")
-            )
-
-            TimeField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
-                time = TextFieldValue("")
-            )
-
-            HeightField(
+            CommonFields(
+                date = TextFieldValue(""),
+                time = TextFieldValue(""),
                 height = TextFieldValue(""),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
-                onHeightChange = {},
-                onDone = {}
-            )
-
-            WeightField(
                 weight = TextFieldValue(""),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
-                onWeightChange = {},
-                onDone = {},
+                focusManager = LocalFocusManager.current,
+                onDateClick = { /*TODO*/ },
+                onTimeClick = { /*TODO*/ },
+                onHeightChange = {},
+                onWeightChange = {}
             )
         }
     }
