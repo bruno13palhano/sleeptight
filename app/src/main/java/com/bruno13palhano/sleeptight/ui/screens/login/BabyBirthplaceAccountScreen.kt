478641257f529692ef13435fc5143382a5069b1d
bruno13palhano <bruno13palhano@gmail.com>
2023-06-17 20:49:17 -0300

    Add topBar and upButton to BabyBirthplaceAccountScreen

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyBirthplaceAccountScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/BabyBirthplaceAccountScreen.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.NavigateNext
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,9 +29,23 @@ import com.bruno13palhano.sleeptight.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BabyBirthplaceAccountScreen(
-    onNextButtonClick: () -> Unit
+    onNextButtonClick: () -> Unit,
+    onNavigationIconClick: () -> Unit
 ) {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.birthplace_label)) },
+                navigationIcon = {
+                    IconButton(onClick = onNavigationIconClick) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = onNextButtonClick) {
                 Icon(
@@ -44,7 +62,7 @@ fun BabyBirthplaceAccountScreen(
                 birthplace = birthplace,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 32.dp, start = 16.dp, end = 16.dp),
+                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
                 onBirthplaceChange = { birthplaceValue -> birthplace = birthplaceValue },
                 onDone = { focusManager.clearFocus() }
             )
@@ -57,6 +75,19 @@ fun BabyBirthplaceAccountScreen(
 @Composable
 fun BabyBirthplaceAccountScreenPreview() {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.birthplace_label)) },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = {}) {
                 Icon(
@@ -72,7 +103,7 @@ fun BabyBirthplaceAccountScreenPreview() {
                 birthplace = birthplace,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 32.dp, start = 16.dp, end = 16.dp),
+                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
                 onBirthplaceChange = { birthplaceValue -> birthplace = birthplaceValue },
                 onDone = {}
             )
