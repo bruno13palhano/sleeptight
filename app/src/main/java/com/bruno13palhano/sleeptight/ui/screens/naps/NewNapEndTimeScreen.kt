deb86ca49258caa4aa02f73d4a65e0ed43164787
bruno13palhano <bruno13palhano@gmail.com>
2023-06-17 17:10:57 -0300

    Add NewNapEndtimeScreen content

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/naps/NewNapEndTimeScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/naps/NewNapEndTimeScreen.kt
@@ -1,18 +1,138 @@
 package com.bruno13palhano.sleeptight.ui.screens.naps
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bruno13palhano.sleeptight.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewNapEndTimeScreen(
-    onDoneButtonClick: () -> Unit
+    onDoneButtonClick: () -> Unit,
+    onNavigationIconClick: () -> Unit
 ) {
-    Column {
-        Text(text = "New Nap End Time Screen")
-        Button(onClick = onDoneButtonClick) {
-            Text(text = "Go to Naps")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.end_time_label)) },
+                navigationIcon = {
+                    IconButton(onClick = onNavigationIconClick) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onDoneButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = stringResource(id = R.string.done_label)
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(top = 32.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .clickable { },
+                imageVector = Icons.Filled.Image,
+                contentDescription = stringResource(id = R.string.end_time_label)
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                text = stringResource(id = R.string.end_time_label),
+                textAlign = TextAlign.Center,
+                fontSize = 22.sp
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun NewNapEndTimeScreenPreview() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.end_time_label)) },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = stringResource(id = R.string.done_label)
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(top = 32.dp)
+                    .align(Alignment.CenterHorizontally),
+                imageVector = Icons.Filled.Image,
+                contentDescription = stringResource(id = R.string.end_time_label)
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                text = stringResource(id = R.string.end_time_label),
+                textAlign = TextAlign.Center,
+                fontSize = 22.sp
+            )
         }
     }
 }
