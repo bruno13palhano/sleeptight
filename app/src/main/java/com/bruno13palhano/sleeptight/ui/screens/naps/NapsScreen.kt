374e372e9d225672924eac91fb451dddbbb5b6f2
bruno13palhano <bruno13palhano@gmail.com>
2023-06-17 16:34:13 -0300

    Add NapsScreen content

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/naps/NapsScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/naps/NapsScreen.kt
@@ -1,23 +1,121 @@
 package com.bruno13palhano.sleeptight.ui.screens.naps
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bruno13palhano.sleeptight.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NapsScreen(
     onItemClick: (napId: Long) -> Unit,
-    onAddButtonClick: () -> Unit
+    onAddButtonClick: () -> Unit,
+    onNavigationIconClick: () -> Unit
 ) {
-    Column {
-        Text(text = "Naps Screen")
-        Button(onClick = { onItemClick(10L) }) {
-            Text(text = "Go to Nap")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.naps_label)) },
+                navigationIcon = {
+                    IconButton(onClick = onNavigationIconClick) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_button)
+                )
+            }
         }
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(35) {
+                ItemTest {
+                    onItemClick(it.toLong())
+                }
+            }
+        }
+    }
+}
 
-        Button(onClick = onAddButtonClick) {
-            Text(text = "Go to New Nap")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun NapsScreenPreview() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.naps_label)) },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_button)
+                )
+            }
         }
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(35) {
+                ItemTest {
+
+                }
+            }
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemTest(
+    onItemClick: () -> Unit
+) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 4.dp, start = 8.dp, end = 8.dp, bottom = 4.dp),
+        onClick = onItemClick
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(16.dp),
+            text = "Item",
+            fontSize = 22.sp
+        )
     }
 }
