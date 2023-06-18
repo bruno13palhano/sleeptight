2ff0e59917bbacd0bee78ee29f28c6cf95fd9812
bruno13palhano <bruno13palhano@gmail.com>
2023-06-18 12:27:06 -0300

    Add BabyStatusListScreen content

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/babystatus/BabyStatusListScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/babystatus/BabyStatusListScreen.kt
@@ -1,24 +1,120 @@
-package com.bruno13palhano.sleeptight.ui.screens.babystatus
+ package com.bruno13palhano.sleeptight.ui.screens.babystatus
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bruno13palhano.sleeptight.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BabyStatusListScreen(
     onItemClick: (babyStatusId: Long) -> Unit,
-    onAddButtonClick: () -> Unit
+    onAddButtonClick: () -> Unit,
+    onNavigationIconClick: () -> Unit
 ) {
-    Column {
-        Text(text = "Baby Status List Screen")
-
-        Button(onClick = { onItemClick(3L) }) {
-            Text(text = "Go to BabyStatus")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.all_baby_status_label)) },
+                navigationIcon = {
+                    IconButton(onClick = onNavigationIconClick) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_button)
+                )
+            }
         }
-
-        Button(onClick = onAddButtonClick) {
-            Text(text = "Go to New Baby Status")
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(35) {
+                ItemBabyTest {
+                    onItemClick(it.toLong())
+                }
+            }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun BabyStatusListScreenPreview() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.all_baby_status_label)) },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_button)
+                )
+            }
+        }
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(35) {
+                ItemBabyTest {
+
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemBabyTest(
+    onItemClick: () -> Unit
+) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 4.dp, start = 8.dp, end = 8.dp, bottom = 4.dp),
+        onClick = onItemClick
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(16.dp),
+            text = "Item",
+            fontSize = 22.sp
+        )
     }
 }
