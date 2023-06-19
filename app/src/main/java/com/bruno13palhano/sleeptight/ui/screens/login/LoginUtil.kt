d7624a938aa1ebf7f71253164ae29f18705315c4
bruno13palhano <bruno13palhano@gmail.com>
2023-06-19 12:39:00 -0300

    Fix decimal inputs

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/LoginUtil.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/LoginUtil.kt
@@ -1,5 +1,6 @@
 package com.bruno13palhano.sleeptight.ui.screens.login
 
+import android.icu.text.DecimalFormat
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -19,6 +20,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -27,6 +29,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import com.bruno13palhano.sleeptight.R
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -262,6 +265,10 @@ fun HeightField(
     onHeightChange: (height: TextFieldValue) -> Unit,
     onDone: () -> Unit
 ) {
+    val decimalFormat = DecimalFormat.getInstance(Locale.getDefault()) as DecimalFormat
+    val decimalSeparator = decimalFormat.decimalFormatSymbols.decimalSeparator
+    val pattern = remember { Regex("^\\d*\\$decimalSeparator?\\d*\$") }
+
     OutlinedTextField(
         modifier = modifier,
         value = height,
@@ -279,8 +286,10 @@ fun HeightField(
             this.defaultKeyboardAction(ImeAction.Done)
             onDone()
         }),
-        onValueChange = { emailValue ->
-            onHeightChange(emailValue)
+        onValueChange = { heightValue ->
+            if (heightValue.text.isEmpty() || heightValue.text.matches(pattern)) {
+                onHeightChange(heightValue)
+            }
         },
         singleLine = true,
         label = { Text(text = stringResource(id = R.string.birth_height_label)) },
@@ -296,6 +305,10 @@ fun WeightField(
     onWeightChange: (weight: TextFieldValue) -> Unit,
     onDone: () -> Unit
 ) {
+    val decimalFormat = DecimalFormat.getInstance(Locale.getDefault()) as DecimalFormat
+    val decimalSeparator = decimalFormat.decimalFormatSymbols.decimalSeparator
+    val pattern = remember { Regex("^\\d*\\$decimalSeparator?\\d*\$") }
+
     OutlinedTextField(
         modifier = modifier,
         value = weight,
@@ -313,8 +326,10 @@ fun WeightField(
             this.defaultKeyboardAction(ImeAction.Done)
             onDone()
         }),
-        onValueChange = { emailValue ->
-            onWeightChange(emailValue)
+        onValueChange = { weightValue ->
+            if (weightValue.text.isEmpty() || weightValue.text.matches(pattern)) {
+                onWeightChange(weightValue)
+            }
         },
         singleLine = true,
         label = { Text(text = stringResource(id = R.string.birth_weight_label)) },
