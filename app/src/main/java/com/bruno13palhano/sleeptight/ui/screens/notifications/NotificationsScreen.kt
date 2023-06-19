eb6270dc60bfe5e38d431487b95a762823f4f174
bruno13palhano <bruno13palhano@gmail.com>
2023-06-18 21:44:30 -0300

    Add content to NotificationsScreen

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/notifications/NotificationsScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/notifications/NotificationsScreen.kt
@@ -1,24 +1,120 @@
 package com.bruno13palhano.sleeptight.ui.screens.notifications
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bruno13palhano.sleeptight.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NotificationsScreen(
     onItemClick: (notificationId: Long) -> Unit,
-    onAddButtonClick: () -> Unit
+    onAddButtonClick: () -> Unit,
+    onNavigationIconClick: () -> Unit
 ) {
-    Column {
-        Text(text = "Notifications Screen")
-
-        Button(onClick = { onItemClick(12L) }) {
-            Text(text = "Go to Notification")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.notifications_label)) },
+                navigationIcon = {
+                    IconButton(onClick = onNavigationIconClick) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_button)
+                )
+            }
         }
-
-        Button(onClick = onAddButtonClick) {
-            Text(text = "Go to New Notification")
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(35) {
+                NotificationItemTest {
+                    onItemClick(it.toLong())
+                }
+            }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun NotificationsScreenPreview() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.notifications_label)) },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_button)
+                )
+            }
+        }
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(35) {
+                NotificationItemTest {
+
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NotificationItemTest(
+    onItemClick: () -> Unit
+) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 4.dp, start = 8.dp, end = 8.dp, bottom = 8.dp),
+        onClick = onItemClick
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(16.dp),
+            text = "Notification Item",
+            fontSize = 22.sp
+        )
     }
 }
