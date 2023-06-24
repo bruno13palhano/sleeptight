61c4ac774305329d4472f6bbc4899da660172135
bruno13palhano <bruno13palhano@gmail.com>
2023-06-24 20:33:54 -0300

    Get a viewModelStoreOwner reference from activity

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/ListsNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/ListsNavGraph.kt
@@ -1,12 +1,16 @@
 package com.bruno13palhano.sleeptight.ui.navigation
 
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.bruno13palhano.sleeptight.ui.screens.ListsScreen
 
-fun NavGraphBuilder.listsNavGraph(navController: NavController) {
+fun NavGraphBuilder.listsNavGraph(
+    navController: NavController,
+    viewModelStoreOwner: ViewModelStoreOwner
+) {
     navigation(
         startDestination = ListsDestinations.ALL_LISTS_ROUTE,
         route = SleepTightDestinations.LISTS_ROUTE
@@ -20,7 +24,7 @@ fun NavGraphBuilder.listsNavGraph(navController: NavController) {
         }
 
         babyStatusNavGraph(navController = navController)
-        napsNavGraph(navController = navController)
+        napsNavGraph(navController = navController, viewModelStoreOwner = viewModelStoreOwner)
         notificationsNavGraph(navController = navController)
     }
 }
--- app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/SleepTightNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/navigation/SleepTightNavGraph.kt
@@ -1,16 +1,12 @@
 package com.bruno13palhano.sleeptight.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.bruno13palhano.sleeptight.ui.screens.AnalyticsScreen
 import com.bruno13palhano.sleeptight.ui.screens.HomeScreen
 import com.bruno13palhano.sleeptight.ui.screens.PlayerScreen
 import com.bruno13palhano.sleeptight.ui.screens.SettingsScreen
@@ -19,7 +15,8 @@ import com.bruno13palhano.sleeptight.ui.screens.SettingsScreen
 fun SleepTightNavGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = SleepTightDestinations.HOME_ROUTE
+    startDestination: String = SleepTightDestinations.HOME_ROUTE,
+    viewModelStoreOwner: ViewModelStoreOwner
 ) {
     NavHost(
         navController = navController,
@@ -30,17 +27,10 @@ fun SleepTightNavGraph(
         loginNavGraph(navController)
 
         composable(route = SleepTightDestinations.HOME_ROUTE) {
-            HomeScreen {
-                if (it)
-                    navController.navigate(SleepTightDestinations.LOGIN_CREATE_ACCOUNT_ROUTE) {
-                        popUpTo(SleepTightDestinations.HOME_ROUTE) {
-                            inclusive = true
-                        }
-                    }
-            }
+            HomeScreen()
         }
 
-        listsNavGraph(navController)
+        listsNavGraph(navController = navController, viewModelStoreOwner = viewModelStoreOwner)
 
         composable(route = SleepTightDestinations.PLAYER_ROUTE) {
             PlayerScreen()
