38060a575757d49d26b93d60958674c928675d3f
bruno13palhano <bruno13palhano@gmail.com>
2023-06-24 22:03:21 -0300

    Refactored

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/naps/NapViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/naps/NapViewModel.kt
@@ -15,8 +15,6 @@ import com.bruno13palhano.model.Nap
 import com.bruno13palhano.sleeptight.ui.util.CalendarUtil
 import com.bruno13palhano.sleeptight.ui.util.DateFormatUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,128 +23,53 @@ class NapViewModel @Inject constructor(
     @DefaultNapRep private val napRepository: NapRepository
 ) : ViewModel() {
 
-    val date = MutableStateFlow(0L)
-    val dateUi = date.asStateFlow()
-        .map {
-            DateFormatUtil.format(it)
-        }
-        .stateIn(
-            scope = viewModelScope,
-            initialValue = "",
-            started = WhileSubscribed(5_000)
-        )
-
-    fun setDate(year: Int, month: Int, day: Int) {
-        date.value = CalendarUtil.dateToMilliseconds(year, month, day)
-    }
-
-    val startTime = MutableStateFlow(0L)
-    val startTimeUi = startTime.asStateFlow()
-        .map {
-            DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(it)
-        }
-        .stateIn(
-            scope = viewModelScope,
-            initialValue = "",
-            started = WhileSubscribed(5_000)
-        )
-
-    fun setStartTime(hour: Int, minute: Int) {
-        startTime.value = CalendarUtil.timeToMilliseconds(hour, minute)
-    }
-
-    val endTime = MutableStateFlow(0L)
-    val endTimeUi = endTime.asStateFlow()
-        .map {
-            DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(it)
-        }
-        .stateIn(
-            scope = viewModelScope,
-            initialValue = "",
-            started = WhileSubscribed(5_000)
-        )
-
-    fun setEndTime(hour: Int, minute: Int) {
-        endTime.value = CalendarUtil.timeToMilliseconds(hour, minute)
-    }
-
-    val title = MutableStateFlow("")
-    val isTitleNotEmpty = title.asStateFlow()
-        .map { it.trim() != "" }
-        .stateIn(
-            scope = viewModelScope,
-            initialValue = false,
-            started = WhileSubscribed(5_000)
-        )
-
-    fun setTitle(title: String) {
-        this.title.value = title
-    }
-
-    var napTitle by mutableStateOf("")
+    var title by mutableStateOf("")
+        private set
+    var dateInMillis by mutableLongStateOf(0L)
+        private set
+    var date by mutableStateOf("")
+        private set
+    var startTimeInMillis by mutableLongStateOf(0L)
+        private set
+    var startTime by mutableStateOf("")
+        private set
+    var startTimeHour by mutableIntStateOf(0)
+        private set
+    var startTimeMinute by mutableIntStateOf(0)
+        private set
+    var endTimeInMillis by mutableLongStateOf(0L)
+        private set
+    var endTime by mutableStateOf("")
+        private set
+    var endTimeHour by mutableIntStateOf(0)
+        private set
+    var endTimeMinute by mutableIntStateOf(0)
+        private set
+    var observation by mutableStateOf("")
         private set
 
     fun updateNapTitle(title: String) {
-        napTitle = title
+        this.title = title
     }
-
-    var dateInMillis by mutableLongStateOf(0L)
-        private set
-    var napDate by mutableStateOf("")
-        private set
 
     fun updateNapDate(date: Long) {
         dateInMillis = date
-        napDate = DateFormatUtil.format(date)
+        this.date = DateFormatUtil.format(date)
     }
 
-    var startTimeInMillis by mutableLongStateOf(0L)
-    var napStartTime by mutableStateOf("")
-        private set
-
-    var napStartTimeHour by mutableIntStateOf(0)
-        private set
-    var napStartTimeMinute by mutableIntStateOf(0)
-        private set
+    fun updateNapObservation(observation: String) {
+        this.observation = observation
+    }
 
     fun updateStartTime(hour: Int, minute: Int) {
         startTimeInMillis = CalendarUtil.timeToMilliseconds(hour, minute)
-        napStartTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(startTimeInMillis)
+        startTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(startTimeInMillis)
     }
-
-    fun updateNapStartTime(startTime: Long) {
-        startTimeInMillis = startTime
-        napStartTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(startTime)
-    }
-
-    var endTimeInMillis by mutableLongStateOf(0L)
-    var napEndTime by mutableStateOf("")
-        private set
-
-    var napEndTimeHour by mutableIntStateOf(0)
-        private set
-
-    var napEndTimeMinute by mutableIntStateOf(0)
-        private set
 
     fun updateEndTime(hour: Int, minute: Int) {
         endTimeInMillis = CalendarUtil.timeToMilliseconds(hour, minute)
-        napEndTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(endTimeInMillis)
+        endTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(endTimeInMillis)
     }
-
-    fun updateNapEndTime(endTime: Long) {
-        endTimeInMillis = endTime
-        napEndTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(endTime)
-    }
-
-    var napObservation by mutableStateOf("")
-        private set
-
-    fun updateNapObservation(observation: String) {
-        napObservation = observation
-    }
-
-    val observation = MutableStateFlow("")
 
     fun getNap(id: Long) {
         viewModelScope.launch {
@@ -156,58 +79,39 @@ class NapViewModel @Inject constructor(
                 updateNapStartTime(it.startTime)
                 updateNapEndTime(it.endTime)
                 updateNapObservation(it.observation)
-                napStartTimeHour = getHour(it.startTime)
-                napStartTimeMinute = getMinute(it.startTime)
-                napEndTimeHour = getHour(it.endTime)
-                napEndTimeMinute = getMinute(it.endTime)
-                title.value = it.title
-                date.value = it.date
-                startTime.value = it.startTime
-                endTime.value = it.endTime
-                observation.value = it.observation
+                startTimeHour = getHour(it.startTime)
+                startTimeMinute = getMinute(it.startTime)
+                endTimeHour = getHour(it.endTime)
+                endTimeMinute = getMinute(it.endTime)
             }
         }
     }
 
-    val uiState = combine(title, date, startTime, endTime, observation) {
-            title, date, startTime, endTime ,observation ->
-        NapUiState(
-            title = title,
-            date = DateFormatUtil.format(date),
-            startTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(startTime),
-            endTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(endTime),
-            observation = observation
-        )
-    }
-        .stateIn(
-            scope = viewModelScope,
-            started = WhileSubscribed(5_000),
-            initialValue = NapUiState()
-        )
-
-    data class NapUiState(
-        val title: String = "",
-        val date: String = "",
-        val startTime: String = "",
-        val endTime: String = "",
-        val observation: String = ""
-    )
-
     fun updateNap(id: Long) {
         val nap = Nap(
             id = id,
-            title = napTitle,
+            title = title,
             date = dateInMillis,
             startTime = startTimeInMillis,
             endTime = endTimeInMillis,
             sleepingTime = CalendarUtil.getSleepTime(startTimeInMillis, endTimeInMillis),
-            observation = napObservation
+            observation = observation
         )
         napRepository.update(nap)
     }
 
     fun deleteNapById(id: Long) {
         napRepository.deleteById(id)
+    }
+
+    private fun updateNapStartTime(startTime: Long) {
+        startTimeInMillis = startTime
+        this.startTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(startTime)
+    }
+
+    private fun updateNapEndTime(endTime: Long) {
+        endTimeInMillis = endTime
+        this.endTime = DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(endTime)
     }
 
     private fun getHour(time: Long): Int {
