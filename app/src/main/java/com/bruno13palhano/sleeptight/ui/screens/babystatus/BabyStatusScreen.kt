d25a5456c74f761bca27017f3bfba5e423b4849e
bruno13palhano <bruno13palhano@gmail.com>
2023-06-25 12:45:05 -0300

    Handle data state from viewModel

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/babystatus/BabyStatusScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/babystatus/BabyStatusScreen.kt
@@ -1,7 +1,6 @@
 package com.bruno13palhano.sleeptight.ui.screens.babystatus
 
 import android.content.res.Configuration
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +17,10 @@ import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Label
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -26,23 +29,26 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.bruno13palhano.sleeptight.R
 import com.bruno13palhano.sleeptight.ui.screens.CommonMenu
+import com.bruno13palhano.sleeptight.ui.screens.CommonMenuItemIndex
 import com.bruno13palhano.sleeptight.ui.screens.login.HeightField
 import com.bruno13palhano.sleeptight.ui.screens.login.WeightField
 
@@ -50,176 +56,168 @@ import com.bruno13palhano.sleeptight.ui.screens.login.WeightField
 @Composable
 fun BabyStatusScreen(
     babyStatusId: Long,
-    onDoneButtonClick: () -> Unit,
-    onNavigationIconClick: () -> Unit
+    navigateUp: () -> Unit,
+    babyStatusViewModel: BabyStatusViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(key1 = null) {
+        babyStatusViewModel.getBabyStatus(babyStatusId)
+    }
+
     val configuration = LocalConfiguration.current
     val focusManager = LocalFocusManager.current
-    var title by remember { mutableStateOf(TextFieldValue("")) }
-    var date by remember { mutableStateOf(TextFieldValue("")) }
-    var height by remember { mutableStateOf(TextFieldValue("")) }
-    var weight by remember { mutableStateOf(TextFieldValue("")) }
 
     var expanded by remember { mutableStateOf(false) }
 
-    when (configuration.orientation) {
-        Configuration.ORIENTATION_PORTRAIT -> {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        title = { Text(text = stringResource(id = R.string.baby_status_label)) },
-                        navigationIcon = {
-                            IconButton(onClick = onNavigationIconClick) {
-                                Icon(
-                                    imageVector = Icons.Filled.ArrowBack,
-                                    contentDescription = stringResource(id = R.string.up_button_label)
-                                )
-                            }
-                        },
-                        actions = {
-                            IconButton(
-                                onClick = { expanded = true }
-                            ) {
-                                Box(contentAlignment = Alignment.Center) {
-                                    Icon(
-                                        imageVector = Icons.Filled.MoreVert,
-                                        contentDescription = stringResource(id = R.string.more_options_label)
-                                    )
+    var showDatePickerDialog by remember { mutableStateOf(false) }
+    var datePickerState = rememberDatePickerState()
 
-                                    Column(
-                                        modifier = Modifier.fillMaxSize(),
-                                        verticalArrangement = Arrangement.Center,
-                                        horizontalAlignment = Alignment.CenterHorizontally
-                                    ) {
-                                        CommonMenu(
-                                            expanded = expanded,
-                                            onDismissRequest = { expandedValue ->
-                                                expanded = expandedValue
-                                            },
-                                            onClick = { println("index: $it") }
-                                        )
-                                    }
-                                }
-                            }
+    if (showDatePickerDialog) {
+        DatePickerDialog(
+            onDismissRequest = {
+                showDatePickerDialog = false
+                focusManager.clearFocus(force = true)
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        datePickerState.selectedDateMillis?.let {
+                            babyStatusViewModel.updateDate(it)
                         }
-                    )
-                },
-                floatingActionButton = {
-                    FloatingActionButton(onClick = onDoneButtonClick) {
+                        showDatePickerDialog = false
+                        focusManager.clearFocus(force = true)
+                    }
+                ) {
+                    Text(text = stringResource(id = R.string.date_label))
+                }
+            }
+        ) {
+            datePickerState = rememberDatePickerState(
+                initialSelectedDateMillis = babyStatusViewModel.dateInMillis,
+                initialDisplayMode = if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    DisplayMode.Picker
+                } else {
+                    DisplayMode.Input
+                }
+            )
+            DatePicker(
+                state = datePickerState,
+                showModeToggle = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+            )
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.baby_status_label)) },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
                         Icon(
-                            imageVector = Icons.Filled.Done,
-                            contentDescription = stringResource(id = R.string.add_button)
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
                         )
                     }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { expanded = true }
+                    ) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = stringResource(id = R.string.more_options_label)
+                            )
+
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                CommonMenu(
+                                    expanded = expanded,
+                                    onDismissRequest = { expandedValue ->
+                                        expanded = expandedValue
+                                    },
+                                    onClick = { index ->
+                                        when (index) {
+                                            CommonMenuItemIndex.DELETE_ITEM_INDEX -> {
+                                                babyStatusViewModel.deleteBabyStatus(babyStatusId)
+                                                navigateUp()
+                                            }
+                                            CommonMenuItemIndex.SHARE_ITEM_INDEX -> {
+
+                                            }
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
                 }
-            ) {
-                Column(modifier = Modifier.padding(it)) {
-                    CommonFields(
-                        title = title,
-                        date = date,
-                        height = height,
-                        weight = weight,
-                        onDateClick = { focusManager.moveFocus(FocusDirection.Next) },
-                        onTitleChange = { titleValue -> title = titleValue },
-                        onHeightChange = { heightValue -> height = heightValue },
-                        onWeightChange = { weightValue -> weight = weightValue },
-                        onTitleDone = { focusManager.moveFocus(FocusDirection.Next) },
-                        onHeightDone = { focusManager.moveFocus(FocusDirection.Next) },
-                        onWeightDone = { focusManager.clearFocus() }
+            )
+        },
+        floatingActionButton = {
+            if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                FloatingActionButton(
+                    onClick = {
+                        babyStatusViewModel.updateBabyStatus(babyStatusId)
+                        navigateUp()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Done,
+                        contentDescription = stringResource(id = R.string.add_button)
                     )
                 }
             }
         }
-        else -> {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        title = { Text(text = stringResource(id = R.string.baby_status_label)) },
-                        navigationIcon = {
-                            IconButton(onClick = onNavigationIconClick) {
-                                Icon(
-                                    imageVector = Icons.Filled.ArrowBack,
-                                    contentDescription = stringResource(id = R.string.up_button_label)
-                                )
-                            }
-                        },
-                        actions = {
-                            IconButton(
-                                onClick = {}
-                            ) {
-                                Box(contentAlignment = Alignment.Center) {
-                                    Icon(
-                                        imageVector = Icons.Filled.MoreVert,
-                                        contentDescription = stringResource(id = R.string.more_options_label)
-                                    )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .verticalScroll(rememberScrollState())
+        ) {
+            CommonFields(
+                title = babyStatusViewModel.title,
+                date = babyStatusViewModel.date,
+                height = babyStatusViewModel.height,
+                weight = babyStatusViewModel.weight,
+                onDateClick = { showDatePickerDialog = true },
+                onTitleChange = babyStatusViewModel::updateTitle,
+                onHeightChange = babyStatusViewModel::updateHeight,
+                onWeightChange = babyStatusViewModel::updateWeight,
+                onTitleDone = { focusManager.clearFocus(force = true) },
+                onHeightDone = { focusManager.clearFocus(force = true) },
+                onWeightDone = { focusManager.clearFocus(force = true) }
+            )
 
-                                    Column(
-                                        modifier = Modifier.fillMaxSize(),
-                                        verticalArrangement = Arrangement.Center,
-                                        horizontalAlignment = Alignment.CenterHorizontally
-                                    ) {
-                                        CommonMenu(
-                                            expanded = expanded,
-                                            onDismissRequest = { expandedValue ->
-                                                expanded = expandedValue
-                                            },
-                                            onClick = { println("index: $it") }
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    )
-                }
-            ) {
-                Column(
+            if (configuration.orientation != Configuration.ORIENTATION_PORTRAIT) {
+                FloatingActionButton(
                     modifier = Modifier
-                        .padding(it)
-                        .fillMaxSize()
-                        .verticalScroll(rememberScrollState())
+                        .align(Alignment.End)
+                        .padding(16.dp),
+                    onClick = navigateUp
                 ) {
-                    CommonFields(
-                        title = title,
-                        date = date,
-                        height = height,
-                        weight = weight,
-                        onDateClick = { focusManager.moveFocus(FocusDirection.Next) },
-                        onTitleChange = { titleValue -> title = titleValue },
-                        onHeightChange = { heightValue -> height = heightValue },
-                        onWeightChange = { weightValue -> weight = weightValue },
-                        onTitleDone = { focusManager.moveFocus(FocusDirection.Next) },
-                        onHeightDone = { focusManager.moveFocus(FocusDirection.Next) },
-                        onWeightDone = { focusManager.clearFocus() }
+                    Icon(
+                        imageVector = Icons.Filled.Done,
+                        contentDescription = stringResource(id = R.string.add_button)
                     )
-
-                    FloatingActionButton(
-                        modifier = Modifier
-                            .align(Alignment.End)
-                            .padding(16.dp),
-                        onClick = onDoneButtonClick
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Done,
-                            contentDescription = stringResource(id = R.string.add_button)
-                        )
-                    }
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CommonFields(
-    title: TextFieldValue,
-    date: TextFieldValue,
-    height: TextFieldValue,
-    weight: TextFieldValue,
+    title: String,
+    date: String,
+    height: String,
+    weight: String,
     onDateClick: () -> Unit,
-    onTitleChange: (title: TextFieldValue) -> Unit,
-    onHeightChange: (height: TextFieldValue) -> Unit,
-    onWeightChange: (weight: TextFieldValue) -> Unit,
+    onTitleChange: (title: String) -> Unit,
+    onHeightChange: (height: String) -> Unit,
+    onWeightChange: (weight: String) -> Unit,
     onTitleDone: () -> Unit,
     onHeightDone: () -> Unit,
     onWeightDone: () -> Unit
@@ -250,7 +248,11 @@ fun CommonFields(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 8.dp, start = 16.dp, end = 16.dp)
-            .clickable { onDateClick() },
+            .onFocusChanged {
+                if (it.hasFocus) {
+                    onDateClick()
+                }
+            },
         value = date,
         readOnly = true,
         leadingIcon = {
@@ -322,10 +324,10 @@ fun BabyStatusScreenPreview() {
     ) {
         Column(modifier = Modifier.padding(it)) {
             CommonFields(
-                title = TextFieldValue(""),
-                date = TextFieldValue(""),
-                height = TextFieldValue(""),
-                weight = TextFieldValue(""),
+                title = "",
+                date = "",
+                height = "",
+                weight = "",
                 onDateClick = {},
                 onTitleChange = {},
                 onHeightChange = {},
