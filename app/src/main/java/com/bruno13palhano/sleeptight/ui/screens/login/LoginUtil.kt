5d5f990ccaa7998ea8e81071e728f66fbd2e6bd7
bruno13palhano <bruno13palhano@gmail.com>
2023-06-26 18:59:11 -0300

    Removed unused import

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/LoginUtil.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/login/LoginUtil.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
 import com.bruno13palhano.sleeptight.R
