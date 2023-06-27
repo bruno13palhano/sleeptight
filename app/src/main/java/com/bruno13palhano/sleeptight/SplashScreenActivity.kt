21a0e3015cc6af8f777343797c3df30f318742a5
bruno13palhano <bruno13palhano@gmail.com>
2023-06-26 21:21:55 -0300

    SlpashScreen redirect to MainActivityCompose

--- app/src/main/java/com/bruno13palhano/sleeptight/SplashScreenActivity.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/SplashScreenActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        startActivity(Intent(this@SplashScreenActivity, MainActivityCompose::class.java))
         finish()
     }
 }
