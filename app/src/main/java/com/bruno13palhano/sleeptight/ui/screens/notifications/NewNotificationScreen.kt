d072a7f856e1363d130c47098a7923368a3732f1
bruno13palhano <bruno13palhano@gmail.com>
2023-06-29 23:26:40 -0300

    Add DatePicker, TimePicker and set alarm

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/notifications/NewNotificationScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/notifications/NewNotificationScreen.kt
@@ -1,7 +1,11 @@
 package com.bruno13palhano.sleeptight.ui.screens.notifications
 
+import android.app.AlarmManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -19,6 +23,10 @@ import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Label
 import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -27,7 +35,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,46 +47,130 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.bruno13palhano.sleeptight.R
+import com.bruno13palhano.sleeptight.ui.screens.TimePickerDialog
+import com.bruno13palhano.sleeptight.ui.screens.notifications.receivers.NotificationReceiver
 
 private const val NOTIFICATION_ACTION_PREFIX = "com.bruno13palhano.sleeptight"
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewNotificationScreen(
     onDoneButtonClick: () -> Unit,
     onNavigationIconClick: () -> Unit,
+    newNotificationViewModel: NewNotificationViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
     val configuration = LocalConfiguration.current
     val focusManager = LocalFocusManager.current
-    var title by remember { mutableStateOf("") }
-    var time by remember { mutableStateOf("") }
-    var date by remember { mutableStateOf("") }
-    var repeat by remember { mutableStateOf(false) }
-    var description by remember { mutableStateOf("") }
+
+    var showDatePickerDialog by remember { mutableStateOf(false) }
+    var datePickerState = rememberDatePickerState()
+    var showTimePickerDialog by remember { mutableStateOf(false) }
+    var timePickerState = rememberTimePickerState()
+
+    if (showDatePickerDialog) {
+        DatePickerDialog(
+            onDismissRequest = {
+                showDatePickerDialog = false
+                focusManager.clearFocus(force = true)
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        datePickerState.selectedDateMillis?.let {
+                            newNotificationViewModel.updateDate(it)
+                        }
+                        showDatePickerDialog = false
+                        focusManager.clearFocus(force = true)
+                    }
+                ) {
+                    Text(text = stringResource(id = R.string.date_label))
+                }
+            }
+        ) {
+            datePickerState = rememberDatePickerState(
+                initialSelectedDateMillis = newNotificationViewModel.dateInMillis,
+                initialDisplayMode = if (isPortraitMode(configuration.orientation)) {
+                    DisplayMode.Picker
+                } else {
+                    DisplayMode.Input
+                }
+            )
+            DatePicker(
+                state = datePickerState,
+                showModeToggle = isPortraitMode(configuration.orientation)
+            )
+        }
+    }
+
+    if (showTimePickerDialog) {
+        TimePickerDialog(
+            title = stringResource(id = R.string.hour_label),
+            onCancelButton = {
+                showTimePickerDialog = false
+                focusManager.clearFocus(force = true)
+            },
+            onConfirmButton = {
+                timePickerState.let {
+                    newNotificationViewModel.updateTime(it.hour, it.minute)
+                }
+                showTimePickerDialog = false
+                focusManager.clearFocus(force = true)
+            }
+        ) {
+            timePickerState = rememberTimePickerState(
+                initialHour = newNotificationViewModel.timeHour,
+                initialMinute = newNotificationViewModel.timeMinute,
+                is24Hour = true
+            )
+
+            if (isPortraitMode(configuration.orientation)) {
+                TimePicker(state = timePickerState)
+            } else {
+                TimeInput(state = timePickerState)
+            }
+        }
+    }
 
     NewNotificationContent(
         orientation = configuration.orientation,
-        title = title,
-        time = time,
-        date = date,
-        repeat = repeat,
-        description = description,
-        onTitleChange = { titleValue -> title = titleValue },
-        onRepeatChange = { repeatValue -> repeat = repeatValue },
-        onDescriptionChange = { descriptionValue -> description = descriptionValue },
+        title = newNotificationViewModel.title,
+        time = newNotificationViewModel.time,
+        date = newNotificationViewModel.date,
+        repeat = newNotificationViewModel.repeat,
+        description = newNotificationViewModel.description,
+        onTitleChange = newNotificationViewModel::updateTitle,
+        onRepeatChange = newNotificationViewModel::updateRepeat,
+        onDescriptionChange = newNotificationViewModel::updateDescription,
         onTitleDone = { focusManager.clearFocus(force = true) },
         onDescriptionDone = { focusManager.clearFocus(force = true) },
-        onTimeClick = { focusManager.clearFocus(force = true) },
-        onDateClick = { focusManager.clearFocus(force = true) },
+        onTimeClick = { showTimePickerDialog = true },
+        onDateClick = { showDatePickerDialog = true },
         onNavigationIconClick = onNavigationIconClick,
-        onDoneButtonClick = onDoneButtonClick
+        onDoneButtonClick = {
+            newNotificationViewModel.insertNotification {
+                setAlarm(
+                    id = it,
+                    title = newNotificationViewModel.title,
+                    time = newNotificationViewModel.timeInMillis,
+                    date = newNotificationViewModel.dateInMillis,
+                    repeat = newNotificationViewModel.repeat,
+                    description = newNotificationViewModel.description,
+                    context = context,
+                )
+            }
+            onDoneButtonClick()
+        }
     )
 }
 
@@ -112,7 +208,7 @@ fun NewNotificationContent(
             )
         },
         floatingActionButton = {
-            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (isPortraitMode(orientation)) {
                 FloatingActionButton(onClick = onDoneButtonClick) {
                     Icon(
                         imageVector = Icons.Filled.Done,
@@ -153,7 +249,11 @@ fun NewNotificationContent(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 8.dp, start = 16.dp, end = 16.dp)
-                    .clickable { onTimeClick() },
+                    .onFocusChanged { focusState ->
+                        if (focusState.hasFocus) {
+                            onTimeClick()
+                        }
+                    },
                 value = time,
                 leadingIcon = {
                     Icon(
@@ -171,7 +271,11 @@ fun NewNotificationContent(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 8.dp, start = 16.dp, end = 16.dp)
-                    .clickable { onDateClick() },
+                    .onFocusChanged { focusState ->
+                        if (focusState.hasFocus) {
+                            onDateClick()
+                        }
+                    },
                 value = date,
                 leadingIcon = {
                     Icon(
@@ -198,23 +302,31 @@ fun NewNotificationContent(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 RadioButton(
-                    selected = repeat,
-                    onClick = { onRepeatChange(!repeat) }
+                    selected = !repeat,
+                    onClick = {
+                        if (repeat) {
+                            onRepeatChange(false)
+                        }
+                    }
                 )
                 Text(
                     text = stringResource(id = R.string.off_label)
                 )
 
                 RadioButton(
-                    selected = !repeat,
-                    onClick = { onRepeatChange(!repeat) }
+                    selected = repeat,
+                    onClick = {
+                        if (!repeat) {
+                            onRepeatChange(true)
+                        }
+                    }
                 )
                 Text(
                     text = stringResource(id = R.string.on_label)
                 )
             }
 
-            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (isPortraitMode(orientation)) {
                 OutlinedTextField(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -307,5 +419,48 @@ fun NewNotificationScreenPreview() {
         onDateClick = {},
         onNavigationIconClick = {},
         onDoneButtonClick = {}
+    )
+}
+
+fun isPortraitMode(orientation: Int): Boolean = orientation == Configuration.ORIENTATION_PORTRAIT
+
+private fun setAlarm(
+    id: Long,
+    title: String,
+    time: Long,
+    date: Long,
+    repeat: Boolean,
+    description: String,
+    context: Context
+) {
+    val notificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+    val notifyIntent = Intent(context, NotificationReceiver::class.java)
+    notifyIntent.apply {
+        action = "$NOTIFICATION_ACTION_PREFIX.${id}"
+        putExtra("id", id.toInt())
+        putExtra("title", title)
+        putExtra("description", description)
+    }
+
+    val notifyPendingIntent = PendingIntent.getBroadcast(
+        context,
+        id.toInt(),
+        notifyIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+
+    val alarmNotification = AlarmNotification(
+        notificationManager = notificationManager,
+        alarmManager = alarmManager
+    )
+
+    alarmNotification.setAlarmManager(
+        notifyPendingIntent = notifyPendingIntent,
+        time = time,
+        date = date,
+        repeat = repeat
     )
 }
