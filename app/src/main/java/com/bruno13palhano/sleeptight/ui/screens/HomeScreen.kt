257d35edbfc0fc0aed5cf36ec48dd582c1bb679f
bruno13palhano <bruno13palhano@gmail.com>
2023-07-06 13:48:24 -0300

    handle home screen state

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/HomeScreen.kt
@@ -2,8 +2,29 @@ package com.bruno13palhano.sleeptight.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
-fun HomeScreen() {
-    Text(text = "Home Screen")
+fun HomeScreen(
+    navigateToLogin: () -> Unit,
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+    val homeState by homeViewModel.homeState.collectAsStateWithLifecycle()
+
+    when (homeState) {
+        HomeViewModel.HomeState.Loading -> {
+            CircularProgress()
+        }
+        HomeViewModel.HomeState.LoggedIn -> {
+            Text(text = "Home Screen")
+        }
+        HomeViewModel.HomeState.NotLoggedIn -> {
+            LaunchedEffect(key1 = Unit) {
+                navigateToLogin()
+            }
+        }
+    }
 }
