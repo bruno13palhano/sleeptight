1f46e03835d352fe00236403fac18d340695ac6e
bruno13palhano <bruno13palhano@gmail.com>
2023-07-09 18:48:31 -0300

    get ChartEntry from viewModel

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/analytics/AnalyticsBabyStatusChartViewModel.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/analytics/AnalyticsBabyStatusChartViewModel.kt
@@ -1,10 +1,15 @@
 package com.bruno13palhano.sleeptight.ui.screens.analytics
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.di.DefaultBabyStatusRep
 import com.bruno13palhano.core.data.repository.BabyStatusRepository
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.entry.FloatEntry
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,14 +26,18 @@ class AnalyticsBabyStatusChartViewModel @Inject constructor(
                 weightList.add(babyStatus.weight)
             }
 
-            BabyStatusChartUi(
-                allHeight = heightList,
-                allWeight = weightList
+            ChartEntryModelProducer(
+                heightList.mapIndexed { index, height ->
+                    FloatEntry(index.toFloat(), height)
+                },
+                weightList.mapIndexed { index, weight ->
+                    FloatEntry(index.toFloat(), weight)
+                }
             )
         }
-
-    data class BabyStatusChartUi(
-        val allHeight: List<Float> = emptyList(),
-        val allWeight: List<Float> = emptyList()
-    )
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = ChartEntryModelProducer()
+        )
 }
