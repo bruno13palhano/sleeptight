5451a1720292d6369046f987f0f04ec11e64c02a
bruno13palhano <bruno13palhano@gmail.com>
2023-07-10 11:09:17 -0300

    Doc StringResourceProvider

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/analytics/StringResourceProvider.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/analytics/StringResourceProvider.kt
@@ -6,11 +6,24 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Class to get String resource
+ *
+ * User this class when you can't get a context reference
+ * e.g. ViewModel classes.
+ *
+ * @property context the ApplicationContext
+ */
 @Singleton
 class StringResourceProvider @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
 
+    /**
+     * Gets a string from StringResource
+     * @param stringId the id from this String Resource
+     * @return a String related to this [stringId]
+     */
     fun getString(@StringRes stringId: Int): String {
         return context.getString(stringId)
     }
