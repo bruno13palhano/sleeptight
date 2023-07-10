7e3bbef64e233824bd6437fa9c2b5ba84ff628db
bruno13palhano <bruno13palhano@gmail.com>
2023-07-10 10:32:51 -0300

    Doc NapChartEntry

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/analytics/NapChartEntry.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/analytics/NapChartEntry.kt
@@ -2,6 +2,16 @@ package com.bruno13palhano.sleeptight.ui.screens.analytics
 
 import com.patrykandpatrick.vico.core.entry.ChartEntry
 
+/**
+ * Custom ChartEntry
+ *
+ * This is a custom implementation of ChartEntry from vico chart library
+ * to handle string label in the x axis.
+ *
+ * @property date a string to represent the date in x axis label.
+ * @property x the index value of x axis.
+ * @property y the value of y axis.
+ */
 class NapChartEntry(
     val date: String,
     override val x: Float,
