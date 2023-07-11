41333fabb182e882a55f50bcec1822ec434515a1
bruno13palhano <bruno13palhano@gmail.com>
2023-07-11 18:52:40 -0300

    Doc BabyStatusDao interface

--- core/src/main/java/com/bruno13palhano/core/data/database/dao/BabyStatusDao.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/dao/BabyStatusDao.kt
@@ -5,18 +5,41 @@ import androidx.room.Query
 import com.bruno13palhano.core.data.database.model.BabyStatusData
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * [BabyStatusData] Dao interface.
+ *
+ * A Data Access Object for the [BabyStatusData] model.
+ * This interface is responsible for handling [BabyStatusData] access to the Room database.
+ */
 @Dao
 internal interface BabyStatusDao : CommonDao<BabyStatusData>{
 
+    /**
+     * Gets all [BabyStatusData].
+     * @return a [Flow] containing a [List] of all [BabyStatusData].
+     */
     @Query("SELECT * FROM baby_status_table")
     fun getAllStream(): Flow<List<BabyStatusData>>
 
+    /**
+     * Gets the [BabyStatusData] specified by this [id].
+     * @param id the [id] for this [BabyStatusData].
+     * @return a [Flow] of [BabyStatusData].
+     */
     @Query("SELECT * FROM baby_status_table WHERE id = :id")
     fun getBabyStatusByIdStream(id: Long): Flow<BabyStatusData>
 
+    /**
+     * Deletes the [BabyStatusData] specified by this [id].
+     * @param id the [id] for this [BabyStatusData].
+     */
     @Query("DELETE FROM baby_status_table WHERE id = :id")
     suspend fun deleteBabyStatusById(id: Long)
 
+    /**
+     * Gets the last [BabyStatusData] inserted into the database.
+     * @return a [Flow] of [BabyStatusData].
+     */
     @Query("SELECT * FROM baby_status_table WHERE id = (SELECT max(id) FROM " +
             "baby_status_table)")
     fun getLastBabyStatusStream(): Flow<BabyStatusData>
