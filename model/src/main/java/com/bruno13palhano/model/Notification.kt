360aa878d966c9edf5545eb661fb16c6907b8d2d
bruno13palhano <bruno13palhano@gmail.com>
2023-07-11 11:21:07 -0300

    Fix typo

--- model/src/main/java/com/bruno13palhano/model/Notification.kt
+++ model/src/main/java/com/bruno13palhano/model/Notification.kt
@@ -6,9 +6,9 @@ package com.bruno13palhano.model
  * @property id the id to identify this Notification.
  * @property title the title of this Notification.
  * @property description the description of this Notification.
- * @property time the time in milliseconds when this notification will start.
- * @property date the date in milliseconds when this notification will start.
- * @property repeat defines if the notification will be repeated every day.
+ * @property time the time in milliseconds when this Notification will start.
+ * @property date the date in milliseconds when this Notification will start.
+ * @property repeat defines if the Notification will be repeated every day.
  */
 data class Notification(
     val id: Long,
