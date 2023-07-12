9528981eac0984b1702aacdd442edb1d716ed93d
bruno13palhano <bruno13palhano@gmail.com>
2023-07-12 20:06:03 -0300

    Extract a contract for common data manipulation

---
+++ core/src/main/java/com/bruno13palhano/core/data/database/data/CommonDataContract.kt
@@ -0,0 +1,21 @@
+package com.bruno13palhano.core.data.database.data
+
+import com.bruno13palhano.model.BabyStatus
+import com.bruno13palhano.model.Nap
+import com.bruno13palhano.model.Notification
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Contract for common data manipulation. e.g [Nap], [BabyStatus], [Notification].
+ *
+ * An interface to decouple the treatment of common data, regardless of concrete implementation.
+ */
+interface CommonDataContract<T> {
+    suspend fun insert(model: T): Long
+    suspend fun update(model: T)
+    suspend fun delete(model: T)
+    suspend fun deleteById(id: Long)
+    fun getAll(): Flow<List<T>>
+    fun getById(id: Long): Flow<T>
+    fun getLast(): Flow<T>
+}
