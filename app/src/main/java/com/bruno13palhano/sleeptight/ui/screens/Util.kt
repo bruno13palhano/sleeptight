3e1e5cc884a2a786dcdf05156aaf8f9f86434a9e
bruno13palhano <bruno13palhano@gmail.com>
2023-07-13 18:42:29 -0300

    Change card background color

--- app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/Util.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/ui/screens/Util.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -173,7 +174,8 @@ fun ItemList(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 8.dp, start = 8.dp, end = 8.dp, bottom = 8.dp),
-        onClick = onItemClick
+        onClick = onItemClick,
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.tertiary)
     ) {
         Box(
             modifier = Modifier.fillMaxSize()
