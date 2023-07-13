a807b97e94aa9eb3458493d2c443defb196a146b
bruno13palhano <bruno13palhano@gmail.com>
2023-07-13 00:32:01 -0300

    Doc CommonDataContract

--- core/src/main/java/com/bruno13palhano/core/data/data/CommonDataContract.kt
+++ core/src/main/java/com/bruno13palhano/core/data/data/CommonDataContract.kt
@@ -12,11 +12,47 @@ import kotlinx.coroutines.flow.Flow
  * @param T the model type.
  */
 interface CommonDataContract<T> {
+
+    /**
+     * Insert a [model] of type [T].
+     * @param model the new [model] to be inserted.
+     */
     suspend fun insert(model: T): Long
+
+    /**
+     * Updates the [model] of type [T].
+     * @param model the [model] to be updated.
+     */
     suspend fun update(model: T)
+
+    /**
+     * Deletes the [model] of type [T].
+     * @param model the [model] to be deleted.
+     */
     suspend fun delete(model: T)
+
+    /**
+     * Deletes the model of type [T] specified by this [id].
+     * @param id the [id] of the model to be deleted.
+     */
     suspend fun deleteById(id: Long)
+
+    /**
+     * Gets all models of type [T].
+     * @return a [Flow] containing a [List] of all models of type [T].
+     */
     fun getAll(): Flow<List<T>>
+
+    /**
+     * Gets the model of type [T] specified by this [id].
+     * @param id the [id] for this model of type [T].
+     * @return a [Flow] containing a model of type [T].
+     */
     fun getById(id: Long): Flow<T>
+
+    /**
+     * Gets the last model [T] inserted.
+     * @return a [Flow] containing a model of type [T].
+     */
     fun getLast(): Flow<T>
 }
