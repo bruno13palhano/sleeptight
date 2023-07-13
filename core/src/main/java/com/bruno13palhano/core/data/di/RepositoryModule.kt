0e2483e9290f9a995338fa93403672eaf8ca6081
bruno13palhano <bruno13palhano@gmail.com>
2023-07-12 22:16:19 -0300

    Change binds to implement UserDataContract and CommonDataContract

--- core/src/main/java/com/bruno13palhano/core/data/di/RepositoryModule.kt
+++ core/src/main/java/com/bruno13palhano/core/data/di/RepositoryModule.kt
@@ -1,8 +1,14 @@
 package com.bruno13palhano.core.data.di
 
+import com.bruno13palhano.core.data.data.CommonDataContract
+import com.bruno13palhano.core.data.data.UserDataContract
 import com.bruno13palhano.core.data.repository.*
 import com.bruno13palhano.core.data.repository.DefaultNapRepository
 import com.bruno13palhano.core.data.repository.DefaultUserRepository
+import com.bruno13palhano.model.BabyStatus
+import com.bruno13palhano.model.Nap
+import com.bruno13palhano.model.Notification
+import com.bruno13palhano.model.User
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -29,20 +35,28 @@ internal abstract class RepositoryModule {
     @DefaultNapRep
     @Singleton
     @Binds
-    abstract fun bindDefaultNapRepository(repository: DefaultNapRepository): NapRepository
+    abstract fun bindDefaultNapRepository(
+        repository: DefaultNapRepository
+    ): CommonDataContract<Nap>
 
     @DefaultUserRep
     @Singleton
     @Binds
-    abstract fun bindDefaultUserRepository(repository: DefaultUserRepository): UserRepository
+    abstract fun bindDefaultUserRepository(
+        repository: DefaultUserRepository
+    ): UserDataContract<User>
 
     @DefaultBabyStatusRep
     @Singleton
     @Binds
-    abstract fun bindDefaultBabyStateRepository(repository: DefaultBabyStatusRepository): BabyStatusRepository
+    abstract fun bindDefaultBabyStateRepository(
+        repository: DefaultBabyStatusRepository
+    ): CommonDataContract<BabyStatus>
 
     @DefaultNotificationRep
     @Singleton
     @Binds
-    abstract fun bindDefaultNotificationRepository(repository: DefaultNotificationRepository): NotificationRepository
+    abstract fun bindDefaultNotificationRepository(
+        repository: DefaultNotificationRepository
+    ): CommonDataContract<Notification>
 }
