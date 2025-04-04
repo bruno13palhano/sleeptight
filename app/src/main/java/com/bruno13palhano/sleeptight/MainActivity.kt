a31a3b390cc658e4f1f81452151cd0940e8fe29f
bruno <bruno13palhano@gmail.com>
2025-04-04 11:51:30 -0300

    Change splash screen implementation

--- app/src/main/java/com/bruno13palhano/sleeptight/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/sleeptight/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.compose.rememberNavController
 import com.bruno13palhano.sleeptight.ui.navigation.BottomMenu
@@ -32,6 +33,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             SleepTightTheme {
                 Surface(
